1713c82841203db0a4e644a8dc366bc37a054e25
Jared Boone <jboone@earfeast.com>
2015-08-14 12:23:49 -0700

    Move ChibiOS init comments into Wiki.

--- firmware/application/main.cpp
+++ firmware/application/main.cpp
@@ -98,34 +98,6 @@ static spi_bus_t ssp0 = {
 };
 #endif
 
-/* ChibiOS initialization sequence:
- * ResetHandler:
- *		Initialize FPU (if present)
- *		Initialize stacks (fill with pattern)
- *		__early_init()
- *			Enable extra processor exceptions for debugging
- *		Init data segment (flash -> data)
- *		Initialize BSS (fill with 0)
- *		__late_init()
- *			reset_peripherals()
- *			halInit()
- *				hal_lld_init()
- *					Init timer 3 as cycle counter
- *					Init RIT as SysTick
- *				palInit()
- *				gptInit()
- *				i2cInit()
- *				sdcInit()
- *				spiInit()
- *				rtcInit()
- *				boardInit()
- *			chSysInit()
- *		Constructors
- *		main()
- *		Destructors
- *		_default_exit() (default is infinite loop)
- */
-
 extern "C" {
 
 CH_IRQ_HANDLER(RTC_IRQHandler) {
