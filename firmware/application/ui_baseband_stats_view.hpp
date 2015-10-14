8f7e26b5c11941406c5c3789faa98bb853c9f0b3
Jared Boone <jboone@earfeast.com>
2015-10-14 10:59:32 -0700

    Break out BasebandStatsView, add main, RSSI threads.

---
+++ firmware/application/ui_baseband_stats_view.hpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2015 Jared Boone, ShareBrained Technology, Inc.
+ *
+ * This file is part of PortaPack.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __UI_BASEBAND_STATS_VIEW_H__
+#define __UI_BASEBAND_STATS_VIEW_H__
+
+#include "ui_widget.hpp"
+#include "message.hpp"
+
+namespace ui {
+
+class BasebandStatsView : public View {
+public:
+	BasebandStatsView();
+
+	void on_show() override;
+	void on_hide() override;
+
+private:
+	Text text_stats {
+		{  0 * 8, 0, (4 * 4 + 3) * 8, 1 * 16 },
+		"",
+	};
+
+	void on_statistics_update(const BasebandStatistics& statistics);
+};
+
+} /* namespace ui */
+
+#endif/*__UI_BASEBAND_STATS_VIEW_H__*/
