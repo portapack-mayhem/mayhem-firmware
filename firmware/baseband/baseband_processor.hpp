c5b234d9f682586dddf2211f7b0cd9ad2567d4ce
Jared Boone <jboone@earfeast.com>
2016-01-04 09:13:17 -0800

    Remove BasebandProcessor virtual method replaced by message.

--- firmware/baseband/baseband_processor.hpp
+++ firmware/baseband/baseband_processor.hpp
@@ -39,7 +39,6 @@ public:
 	virtual void execute(const buffer_c8_t& buffer) = 0;
 
 	virtual void on_message(const Message* const) { };
-	virtual void on_update_spectrum() { };
 
 protected:
 	void feed_channel_stats(const buffer_c16_t& channel);
