ab2c5b256f244cdd2a8fefb46fd4bb83db36b9e0
Jared Boone <jboone@earfeast.com>
2016-02-02 16:08:18 -0800

    Reinstate and slightly tweak NBFM squelch.

--- firmware/baseband/proc_nfm_audio.cpp
+++ firmware/baseband/proc_nfm_audio.cpp
@@ -77,7 +77,7 @@ void NarrowbandFMAudio::configure(const NBFMConfigureMessage& message) {
 	channel_filter_pass_f = message.channel_filter.pass_frequency_normalized * channel_filter_input_fs;
 	channel_filter_stop_f = message.channel_filter.stop_frequency_normalized * channel_filter_input_fs;
 	channel_spectrum.set_decimation_factor(std::floor((channel_filter_output_fs / 2) / ((channel_filter_pass_f + channel_filter_stop_f) / 2)));
-	audio_output.configure(message.audio_hpf_config, message.audio_deemph_config, 0);
+	audio_output.configure(message.audio_hpf_config, message.audio_deemph_config, 12288);
 
 	configured = true;
 }
