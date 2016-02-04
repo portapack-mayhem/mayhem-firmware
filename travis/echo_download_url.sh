2e7dbe6d44ba570ba2bb375f4ca1410c8a96af4b
Jared Boone <jboone@earfeast.com>
2016-02-04 11:18:03 -0800

    Final Travis fiddling for now, I think.

---
+++ travis/echo_download_url.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo -e "########################################################################\n\n"
+echo -e "Correct download URL is:\n"
+echo -e "https://portapack-h1-builds.s3.amazonaws.com/$TRAVIS_REPO_SLUG/$TRAVIS_BUILD_NUMBER/$TRAVIS_JOB_NUMBER/firmware/portapack-h1-firmware-$TRAVIS_COMMIT.tar.bz2\n\n"
+echo -e "########################################################################\n"
