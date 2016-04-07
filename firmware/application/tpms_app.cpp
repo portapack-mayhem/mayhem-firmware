635b1fd4c3f6a18fcb86f6603db90240aab8f49f
Jared Boone <jboone@earfeast.com>
2016-04-06 17:01:54 -0700

    Extract TPMS packet stuff to separate files.

--- firmware/application/tpms_app.cpp
+++ firmware/application/tpms_app.cpp
@@ -27,8 +27,6 @@
 
 #include "string_format.hpp"
 
-#include "crc.hpp"
-
 #include "utility.hpp"
 
 namespace tpms {
@@ -52,87 +50,6 @@ std::string temperature(Temperature temperature) {
 }
 
 } /* namespace format */
-
-Timestamp Packet::received_at() const {
-	return packet_.timestamp();
-}
-
-ManchesterFormatted Packet::symbols_formatted() const {
-	return format_manchester(decoder_);
-}
-
-Optional<Reading> Packet::reading() const {
-	const auto length = crc_valid_length();
-
-	switch(length) {
-	case 64:
-		return Reading {
-			Reading::Type::FLM_64,
-			reader_.read(0, 32),
-			Pressure { static_cast<int>(reader_.read(32, 8)) * 4 / 3 },
-			Temperature { static_cast<int>(reader_.read(40, 8) & 0x7f) - 50 }
-		};
-
-	case 72:
-		return Reading {
-			Reading::Type::FLM_72,
-			reader_.read(0, 32),
-			Pressure { static_cast<int>(reader_.read(40, 8)) * 4 / 3 },
-			Temperature { static_cast<int>(reader_.read(48, 8)) - 50 }
-		};
-
-	case 80:
-		return Reading {
-			Reading::Type::FLM_80,
-			reader_.read(8, 32),
-			Pressure { static_cast<int>(reader_.read(48, 8)) * 4 / 3 },
-			Temperature { static_cast<int>(reader_.read(56, 8)) - 50 }
-		};
-
-	default:
-		return { };
-	}
-}
-
-size_t Packet::crc_valid_length() const {
-	constexpr uint32_t checksum_bytes = 0b1111111;
-	constexpr uint32_t crc_72_bytes = 0b111111111;
-	constexpr uint32_t crc_80_bytes = 0b1111111110;
-
-	std::array<uint8_t, 10> bytes;
-	for(size_t i=0; i<bytes.size(); i++) {
-		bytes[i] = reader_.read(i * 8, 8);
-	}
-
-	uint32_t checksum = 0;
-	CRC<8> crc_72 { 0x01, 0x00 };
-	CRC<8> crc_80 { 0x01, 0x00 };
-
-	for(size_t i=0; i<bytes.size(); i++) {
-		const uint32_t byte_mask = 1 << i;
-		const auto byte = bytes[i];
-
-		if( checksum_bytes & byte_mask ) {
-			checksum += byte;
-		}
-		if( crc_72_bytes & byte_mask ) {
-			crc_72.process_byte(byte);
-		}
-		if( crc_80_bytes & byte_mask ) {
-			crc_80.process_byte(byte);
-		}
-	}
-
-	if( crc_80.checksum() == 0 ) {
-		return 80;
-	} else if( crc_72.checksum() == 0 ) {
-		return 72;
-	} else if( (checksum & 0xff) == bytes[7] ) {
-		return 64;
-	} else {
-		return 0;
-	}
-}
 
 } /* namespace tpms */
 
