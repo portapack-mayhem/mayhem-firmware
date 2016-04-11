9e1b88d85fcbdb9ff30e39e30b63a883735df86f
Jared Boone <jboone@earfeast.com>
2016-04-10 17:27:36 -0700

    Enable FATFS fast seek feature.
    Handy when appending to large baseband files.

--- firmware/application/ffconf.h
+++ firmware/application/ffconf.h
@@ -50,7 +50,7 @@
 /  To enable it, also _FS_READONLY need to be set to 0. */
 
 
-#define	_USE_FASTSEEK	0
+#define	_USE_FASTSEEK	1
 /* This option switches fast seek feature. (0:Disable or 1:Enable) */
 
 
