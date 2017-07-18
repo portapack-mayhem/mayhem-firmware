2356b585d5002ae01523e94753205283165d433f
Jared Boone <jboone@earfeast.com>
2017-07-18 10:37:29 -0700

    FatFs: Fix Makefiles to include unicode.c.

--- firmware/chibios-portapack/os/various/fatfs_bindings/fatfs.mk
+++ firmware/chibios-portapack/os/various/fatfs_bindings/fatfs.mk
@@ -1,6 +1,7 @@
 # FATFS files.
 FATFSSRC = ${CHIBIOS_PORTAPACK}/os/various/fatfs_bindings/fatfs_diskio.c \
            ${CHIBIOS_PORTAPACK}/os/various/fatfs_bindings/fatfs_syscall.c \
-           ${CHIBIOS_PORTAPACK}/ext/fatfs/src/ff.c
+           ${CHIBIOS_PORTAPACK}/ext/fatfs/src/ff.c \
+           ${CHIBIOS_PORTAPACK}/ext/fatfs/src/option/unicode.c
 
 FATFSINC = ${CHIBIOS_PORTAPACK}/ext/fatfs/src
--- firmware/chibios/os/various/fatfs_bindings/fatfs.mk
+++ firmware/chibios/os/various/fatfs_bindings/fatfs.mk
@@ -2,6 +2,6 @@
 FATFSSRC = ${CHIBIOS}/os/various/fatfs_bindings/fatfs_diskio.c \
            ${CHIBIOS}/os/various/fatfs_bindings/fatfs_syscall.c \
            ${CHIBIOS}/ext/fatfs/src/ff.c \
-           ${CHIBIOS}/ext/fatfs/src/option/ccsbcs.c
+           ${CHIBIOS}/ext/fatfs/src/option/unicode.c
 
 FATFSINC = ${CHIBIOS}/ext/fatfs/src
