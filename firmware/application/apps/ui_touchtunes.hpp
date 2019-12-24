8696146fab9a477bff29f39694fc6887774f3083
Furrtek <furrtek@gmail.com>
2019-12-24 07:06:01 +0100

    Contact info removal

--- firmware/application/apps/ui_touchtunes.hpp
+++ firmware/application/apps/ui_touchtunes.hpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2015 Jared Boone, ShareBrained Technology, Inc.
  * Copyright (C) 2017 Furrtek
- * Copyright (C) 2018 NotPike (notpike@horsefucker.org)
+ * Copyright (C) 2018 NotPike
  *
  * This file is part of PortaPack.
  *
