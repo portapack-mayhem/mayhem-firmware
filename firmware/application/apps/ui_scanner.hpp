65d838d4e5a47c06f56f8c6052294a1e01c723df
alainD <65554859+alain00091@users.noreply.github.com>
2020-06-17 07:54:22 +0200

    wrong directory   sorry

--- firmware/application/apps/ui_scanner.hpp
+++ firmware/application/apps/ui_scanner.hpp
@@ -27,7 +27,7 @@
 #include "freqman.hpp"
 
 namespace ui {
-
+	
 class ScannerThread {
 public:
 	ScannerThread(std::vector<rf::Frequency> frequency_list);
@@ -51,48 +51,54 @@ private:
 	void run();
 };
 
-class ScannerView : public View {
+class ScannerView_NFM : public View {
 public:
-	ScannerView(NavigationView&);
-	~ScannerView();
+	ScannerView_NFM(NavigationView&);
+	~ScannerView_NFM();
 	
 	void focus() override;
 	
-	std::string title() const override { return "Scanner"; };
+	std::string title() const override { return "Scanner NFM"; };
 
 private:
 	void on_statistics_update(const ChannelStatistics& statistics);
 	void on_headphone_volume_changed(int32_t v);
 	void handle_retune(uint32_t i);
 	
-	std::vector<rf::Frequency> frequency_list { };
+	
+	std::vector<rf::Frequency> frequency_list{ };
 	std::vector<string> description_list { };
-	int32_t trigger { 0 };
+
+	
 	int32_t squelch { 0 };
 	uint32_t timer { 0 };
 	uint32_t wait { 0 };
 	freqman_db database { };
 	
 	Labels labels {
-		{ { 0 * 8, 0 * 16 }, "LNA:    TRIGGER:  /99   VOL:", Color::light_grey() },
-		{ { 0 * 8, 1 * 16 }, "VGA:    SQUELCH:  /99   AMP:", Color::light_grey() },
-		{ { 0 * 8, 2 * 16 }, " BW:       WAIT:", Color::light_grey() },
+		{ { 0 * 8, 0 * 16 }, "LNA:   VGA:   AMP:  VOL:", Color::light_grey() },
+		{ { 0 * 8, 1 * 16 }, "BW:    SQUELCH:  /99 WAIT:", Color::light_grey() },
 	};
+	
+	//   x1, 0 = debut de ligne
+		//   y1, 50 =hauteur
+		//   x2, 15 = longueur de la ligne
+		//	y2 , 8 epaisseur ligne
 	
 	LNAGainField field_lna {
 		{ 4 * 8, 0 * 16 }
 	};
 
 	VGAGainField field_vga {
-		{ 4 * 8, 1 * 16 }
+		{ 11 * 8, 0 * 16 }
 	};
 	
 	RFAmpField field_rf_amp {
-		{ 28 * 8, 1 * 16 }
+		{ 18 * 8, 0 * 16 }
 	};
 	
 	NumberField field_volume {
-		{ 28 * 8, 0 * 16 },
+		{ 24 * 8, 0 * 16 },
 		2,
 		{ 0, 99 },
 		1,
@@ -100,8 +106,8 @@ private:
 	};
 
 	OptionsField field_bw {
-		{ 4 * 8, 2 * 16 },
-		3,
+		{ 3 * 8, 1 * 16 },
+		4,
 		{
 			{ "8k5", 0 },
 			{ "11k", 0 },
@@ -109,16 +115,8 @@ private:
 		}
 	};
 
-	NumberField field_trigger {
-		{ 16 * 8, 0 * 16 },
-		2,
-		{ 0, 99 },
-		1,
-		' ',
-	};
-	
 	NumberField field_squelch {
-		{ 16 * 8, 1 * 16 },
+		{ 15 * 8, 1 * 16 },
 		2,
 		{ 0, 99 },
 		1,
@@ -126,7 +124,7 @@ private:
 	};
 
 	NumberField field_wait {
-		{ 16 * 8, 2 * 16 },
+		{ 26 * 8, 1 * 16 },
 		2,
 		{ 0, 99 },
 		1,
@@ -135,12 +133,24 @@ private:
 
 	Text text_cycle {
 		{ 0, 5 * 16, 240, 16 },
-		"--/--"
 	};
+	
+ Text text_cycle1 {
+		{ 0, 9 * 16, 240, 16 },
+	};
+	
 	Text desc_cycle {
 		{0, 6 * 16, 240, 16 },
-		" "
+			   
 	};
+	
+Text text_modulation {
+		{ 0, 12 * 16, 240, 16 },
+	}; 
+	
+	RSSI rssi {
+		{ 0 * 16, 50, 15 * 16, 8 },
+	}; 
 	
 	std::unique_ptr<ScannerThread> scan_thread { };
 	
@@ -159,5 +169,243 @@ private:
 		}
 	};
 };
+
+class ScannerView_AM : public View {
+public:
+	ScannerView_AM(NavigationView&);
+	~ScannerView_AM();
+	
+	void focus() override;
+	
+	std::string title() const override { return "Scanner AM"; };
+
+private:
+	void on_statistics_update(const ChannelStatistics& statistics);
+	void on_headphone_volume_changed(int32_t v);
+	void handle_retune(uint32_t i);
+
+std::vector<rf::Frequency> frequency_list{ };
+std::vector<string> description_list { };	
+	
+	int32_t squelch { 0 };
+	uint32_t timer { 0 };
+	uint32_t wait { 0 };
+	freqman_db database { };
+	
+	Labels labels {
+		{ { 0 * 8, 0 * 16 }, "LNA:   VGA:   AMP:  VOL:", Color::light_grey() },
+		{ { 0 * 8, 1 * 16 }, "BW:    SQUELCH:  /99 WAIT:", Color::light_grey() },
+	};
+	
+	LNAGainField field_lna {
+		{ 4 * 8, 0 * 16 }
+	};
+
+	VGAGainField field_vga {
+		{ 11 * 8, 0 * 16 }
+	};
+	
+	RFAmpField field_rf_amp {
+		{ 18 * 8, 0 * 16 }
+	};
+	
+	NumberField field_volume {
+		{ 24 * 8, 0 * 16 },
+		2,
+		{ 0, 99 },
+		1,
+		' ',
+	};
+
+	OptionsField field_bw {
+		{ 3 * 8, 1 * 16 },
+		4,
+		{
+			{ "DSB ", 0 },
+			{ "USB ", 0 },
+			{ "LSB ", 0 },
+		}
+	};
+
+	NumberField field_squelch {
+		{ 15 * 8, 1 * 16 },
+		2,
+		{ 0, 99 },
+		1,
+		' ',
+	};
+
+	NumberField field_wait {
+		{ 26 * 8, 1 * 16 },
+		2,
+		{ 0, 99 },
+		1,
+		' ',
+	};
+
+	Text text_cycle {
+		{ 0, 5 * 16, 240, 16 },
+		
+	};
+	
+ Text text_cycle1 {
+		{ 0, 9 * 16, 240, 16 },
+	};
+	
+	Text desc_cycle {
+		{0, 6 * 16, 240, 16 },
+			   
+	};
+	
+	Text text_modulation {
+		{ 0, 12 * 16, 240, 16 },
+	};
+
+	RSSI rssi {
+		{ 0 * 16, 50, 15 * 16, 8 },
+		
+		
+		
+		
+	}; 
+	
+	std::unique_ptr<ScannerThread> scan_thread { };
+	
+	MessageHandlerRegistration message_handler_retune {
+		Message::ID::Retune,
+		[this](const Message* const p) {
+			const auto message = *reinterpret_cast<const RetuneMessage*>(p);
+			this->handle_retune(message.range);
+		}
+	};
+	
+	MessageHandlerRegistration message_handler_stats {
+		Message::ID::ChannelStatistics,
+		[this](const Message* const p) {
+			this->on_statistics_update(static_cast<const ChannelStatisticsMessage*>(p)->statistics);
+		}
+	};
+};
+
+
+
+class ScannerView_FM : public View {
+public:
+	ScannerView_FM(NavigationView&);
+	~ScannerView_FM();
+	
+	void focus() override;
+	
+	std::string title() const override { return "Scanner FM"; };
+
+private:
+	void on_statistics_update(const ChannelStatistics& statistics);
+	void on_headphone_volume_changed(int32_t v);
+	void handle_retune(uint32_t i);
+	
+	std::vector<rf::Frequency> frequency_list{ };
+	std::vector<string> description_list { };
+	
+	
+	int32_t squelch { 0 };
+	uint32_t timer { 0 };
+	uint32_t wait { 0 };
+	freqman_db database { };
+	
+	Labels labels {
+		{ { 0 * 8, 0 * 16 }, "LNA:   VGA:   AMP:  VOL:", Color::light_grey() },
+		{ { 0 * 8, 1 * 16 }, "BW:    SQUELCH:  /99 WAIT:", Color::light_grey() },
+	};
+	
+	LNAGainField field_lna {
+		{ 4 * 8, 0 * 16 }
+	};
+
+	VGAGainField field_vga {
+		{ 11 * 8, 0 * 16 }
+	};
+	
+	RFAmpField field_rf_amp {
+		{ 18 * 8, 0 * 16 }
+	};
+	
+	NumberField field_volume {
+		{ 24 * 8, 0 * 16 },
+		2,
+		{ 0, 99 },
+		1,
+		' ',
+	};
+
+	OptionsField field_bw {
+		{ 3 * 8, 1 * 16 },
+		4,
+		{
+		/*	{ "3k", 0 },
+			{ "6k", 0 },
+			{ "8k5", 0 },
+			{ "11k", 0 }, */
+			{ "16k", 0 },
+		}
+	};
+	NumberField field_squelch {
+		{ 15 * 8, 1 * 16 },
+		2,
+		{ 0, 99 },
+		1,
+		' ',
+	};
+
+	NumberField field_wait {
+		{ 26 * 8, 1 * 16 },
+		2,
+		{ 0, 99 },
+		1,
+		' ',
+	};
+
+	Text text_cycle {
+		{ 0, 5 * 16, 240, 16 },
+		
+	};
+	
+	Text text_cycle1 {
+		{ 0, 9 * 16, 240, 16 },
+		
+	};
+	
+	Text desc_cycle {
+		{0, 6 * 16, 240, 16 },
+		  
+	};
+	
+	Text text_modulation {
+		{ 0, 12 * 16, 240, 16 },
+	};
+	
+	
+	RSSI rssi {
+		{ 0 * 16, 50, 15 * 16, 8 },
+	}; 
+	
+	std::unique_ptr<ScannerThread> scan_thread { };
+	
+	MessageHandlerRegistration message_handler_retune {
+		Message::ID::Retune,
+		[this](const Message* const p) {
+			const auto message = *reinterpret_cast<const RetuneMessage*>(p);
+			this->handle_retune(message.range);
+		}
+	};
+	
+	MessageHandlerRegistration message_handler_stats {
+		Message::ID::ChannelStatistics,
+		[this](const Message* const p) {
+			this->on_statistics_update(static_cast<const ChannelStatisticsMessage*>(p)->statistics);
+		}
+	};
+};
+
+													 
 
 } /* namespace ui */
