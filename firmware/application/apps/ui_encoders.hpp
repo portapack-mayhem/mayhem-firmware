2436b321cb57896639babcd89c61bde391433de0
euquiq <31453004+euquiq@users.noreply.github.com>
2020-09-11 16:17:10 -0300

    OOK App simplified and De Bruijn button added
    The new OOK App works like before, but no tabs, since the De Bruijn scan is just a button.
    
    The De Bruijn Scan needs further validation. The De Bruijn algorithm implementation is the original one from HAVOC version, and I think it is flawed.

--- firmware/application/apps/ui_encoders.hpp
+++ firmware/application/apps/ui_encoders.hpp
@@ -21,7 +21,6 @@
  */
 
 #include "ui.hpp"
-#include "ui_tabview.hpp"
 #include "ui_transmitter.hpp"
 #include "transmitter_model.hpp"
 #include "encoders.hpp"
@@ -31,134 +30,25 @@ using namespace encoders;
 
 namespace ui {
 
-class EncodersConfigView : public View {
-public:
-	EncodersConfigView(NavigationView& nav, Rect parent_rect);
-	
-	EncodersConfigView(const EncodersConfigView&) = delete;
-	EncodersConfigView(EncodersConfigView&&) = delete;
-	EncodersConfigView& operator=(const EncodersConfigView&) = delete;
-	EncodersConfigView& operator=(EncodersConfigView&&) = delete;
-	
-	void focus() override;
-	void on_show() override;
-	
-	uint8_t repeat_min();
-	uint32_t samples_per_bit();
-	uint32_t pause_symbols();
-	void generate_frame();
-	
-	std::string frame_fragments = "0";
-
-private:
-	//bool abort_scan = false;
-	//uint8_t scan_count;
-	//double scan_progress;
-	//unsigned int scan_index;
-	int16_t waveform_buffer[550];
-	const encoder_def_t * encoder_def { };
-	//uint8_t enc_type = 0;
-
-	void draw_waveform();
-	void on_bitfield();
-	void on_type_change(size_t index);
-	
-	Labels labels {
-		{ { 1 * 8, 0 }, "Type:", Color::light_grey() },
-		{ { 16 * 8, 0 }, "Clk:", Color::light_grey() },
-		{ { 24 * 8, 0 }, "kHz", Color::light_grey() },
-		{ { 14 * 8, 2 * 8 }, "Frame:", Color::light_grey() },
-		{ { 26 * 8, 2 * 8 }, "us", Color::light_grey() },
-		{ { 2 * 8, 4 * 8 }, "Symbols:", Color::light_grey() },
-		{ { 1 * 8, 11 * 8 }, "Waveform:", Color::light_grey() }
-	};
-
-	OptionsField options_enctype {		// Options are loaded at runtime
-		{ 6 * 8, 0 },
-		7,
-		{
-		}
-	};
-
-	NumberField field_clk {
-		{ 21 * 8, 0 },
-		3,
-		{ 1, 500 },
-		1,
-		' '
-	};
-
-	NumberField field_frameduration {
-		{ 21 * 8, 2 * 8 },
-		5,
-		{ 300, 99999 },
-		100,
-		' '
-	};
-	
-	SymField symfield_word {
-		{ 2 * 8, 6 * 8 },
-		20,
-		SymField::SYMFIELD_DEF
-	};
-	
-	Text text_format {
-		{ 2 * 8, 8 * 8, 24 * 8, 16 },
-		""
-	};
-	
-	Waveform waveform {
-		{ 0, 14 * 8, 240, 32 },
-		waveform_buffer,
-		0,
-		0,
-		true,
-		Color::yellow()
-	};
-};
-
-
-class EncodersScanView : public View {
-public:
-	EncodersScanView(NavigationView& nav, Rect parent_rect);
-	
-	void focus() override;
-
-private:
-	Labels labels {
-		{ { 1 * 8, 1 * 8 }, "Coming soon...", Color::light_grey() }
-	};
-	
-	// DEBUG
-	NumberField field_debug {
-		{ 1 * 8, 6 * 8 },
-		2,
-		{ 3, 16 },
-		1,
-		' '
-	};
-	
-	// DEBUG
-	Text text_debug {
-		{ 1 * 8, 8 * 8, 24 * 8, 16 },
-		""
-	};
-	
-	// DEBUG
-	Text text_length {
-		{ 1 * 8, 10 * 8, 24 * 8, 16 },
-		""
-	};
-};
-
 class EncodersView : public View {
 public:
 	EncodersView(NavigationView& nav);
 	~EncodersView();
 	
+
+	EncodersView(NavigationView& nav, Rect parent_rect);
+	
+	EncodersView(const EncodersView&) = delete;
+	EncodersView(EncodersView&&) = delete;
+	EncodersView& operator=(const EncodersView&) = delete;
+	EncodersView& operator=(EncodersView&&) = delete;
+
 	void focus() override;
 	
 	std::string title() const override { return "OOK transmit"; };
+
+
+
 
 private:
 	NavigationView& nav_;
@@ -168,39 +58,99 @@ private:
 		SINGLE,
 		SCAN
 	};
+
+	uint32_t samples_per_bit();
+	uint32_t pause_symbols();
+	void generate_frame(bool is_debruijn, uint32_t debruijn_bits);
+	
+	std::string frame_fragments = "0";
+	const encoder_def_t * encoder_def { };
+	uint8_t bits_per_packet; 	//Euquiq: the number of bits needed from de_bruijn, depends on the encoder's needs
 	
 	tx_modes tx_mode = IDLE;
 	uint8_t repeat_index { 0 };
-	uint8_t repeat_min { 0 };
+	uint32_t scan_count;
+	uint32_t scan_index;
+	double scan_progress;
+	bool abort_scan = false;
+	char str[16];
+
+	uint8_t afsk_repeats;
+	de_bruijn debruijn_seq;
 	
 	void update_progress();
 	void start_tx(const bool scan);
 	void on_tx_progress(const uint32_t progress, const bool done);
+
+	int16_t waveform_buffer[550];
 	
-	/*const Style style_address {
-		.font = font::fixed_8x16,
-		.background = Color::black(),
-		.foreground = Color::red(),
+	uint8_t enc_type = 0;
+
+	void draw_waveform();
+	void on_bitfield();
+	void on_type_change(size_t index);
+
+	Labels labels {
+		{ { 1 * 8, 1 * 8}, "Type:", Color::light_grey() },
+		{ { 16 * 8, 1 * 8}, "Clk:", Color::light_grey() },
+		{ { 24 * 8, 1 * 8}, "kHz", Color::light_grey() },
+		{ { 14 * 8, 3 * 8 }, "Frame:", Color::light_grey() },
+		{ { 26 * 8, 3 * 8 }, "us", Color::light_grey() },
+		{ { 2 * 8, 5 * 8 }, "Symbols:", Color::light_grey() },
+		{ { 1 * 8, 13 * 8 }, "Waveform:", Color::light_grey() }
 	};
-	const Style style_data {
-		.font = font::fixed_8x16,
-		.background = Color::black(),
-		.foreground = Color::blue(),
-	};*/
-	
-	Rect view_rect = { 0, 4 * 8, 240, 168 };
-	
-	EncodersConfigView view_config { nav_, view_rect };
-	EncodersScanView view_scan { nav_, view_rect };
-	
-	TabView tab_view {
-		{ "Config", Color::cyan(), &view_config },
-		{ "Scan", Color::green(), &view_scan },
+
+	OptionsField options_enctype {		// Options are loaded at runtime
+		{ 6 * 8, 1 * 8 },
+		7,
+		{
+		}
 	};
+
+	NumberField field_clk {
+		{ 21 * 8, 1 * 8 },
+		3,
+		{ 1, 500 },
+		1,
+		' '
+	};
+
+	NumberField field_frameduration {
+		{ 21 * 8, 3 * 8 },
+		5,
+		{ 300, 99999 },
+		100,
+		' '
+	};
+	
+	SymField symfield_word {
+		{ 2 * 8, 7 * 8 },
+		20,
+		SymField::SYMFIELD_DEF
+	};
+	
+	Text text_format {
+		{ 2 * 8, 9 * 8, 24 * 8, 16 },
+		""
+	};
+	
+	Waveform waveform {
+		{ 0, 15 * 8, 240, 32 },
+		waveform_buffer,
+		0,
+		0,
+		true,
+		Color::yellow()
+	};	
 
 	Text text_status {
 		{ 2 * 8, 13 * 16, 128, 16 },
 		"Ready"
+	};
+
+	Button button_scan {
+		{ 8 * 8, 11 * 16, 120, 28 },
+		"DE BRUIJN TX"
 	};
 	
 	ProgressBar progressbar {
