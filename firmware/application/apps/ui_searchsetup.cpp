a4d540c361369f12ec246c95154134cbb605109d
GullCode <gullradriel@hotmail.com>
2020-11-11 17:56:44 +0100

    Search Settings files

---
+++ firmware/application/apps/ui_searchsetup.cpp
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2015 Jared Boone, ShareBrained Technology, Inc.
+ * Copyright (C) 2016 Furrtek
+ *
+ * This file is part of PortaPack.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "ui_searchsetup.hpp"
+
+#include "portapack.hpp"
+#include "portapack_persistent_memory.hpp"
+
+using namespace portapack;
+
+namespace ui {
+
+void SearchSetupView::focus() {
+	field_baudrate.focus();
+}
+
+SearchSetupView::SearchSetupView(
+	NavigationView& nav
+)
+{
+	using option_t = std::pair<std::string, int32_t>;
+	using options_t = std::vector<option_t>;
+	options_t search_options;
+	
+	add_children({
+		&labels,
+		&field_baudrate,
+		&field_mark,
+		&field_space,
+		&field_repeat,
+		&options_search,
+		&button_set_search,
+		&sym_format,
+		&button_save
+	});
+	
+	options_search.set_options(search_options);
+	options_search.set_selected_index(0);
+	
+	sym_format.set_symbol_list(0, "6789");		// Data bits
+	sym_format.set_symbol_list(1, "NEo");		// Parity
+	sym_format.set_symbol_list(2, "012");		// Stop bits
+	sym_format.set_symbol_list(3, "ML");		// MSB/LSB first
+	
+	sym_format.set_sym(0, persistent_memory::serial_format().data_bits - 6);
+	sym_format.set_sym(1, persistent_memory::serial_format().parity);
+	sym_format.set_sym(2, persistent_memory::serial_format().stop_bits);
+	sym_format.set_sym(3, persistent_memory::serial_format().bit_order);
+	
+	field_mark.set_value(persistent_memory::afsk_mark_freq());
+	field_space.set_value(persistent_memory::afsk_space_freq());
+	field_repeat.set_value(persistent_memory::search_repeat());
+	
+	field_baudrate.set_value(persistent_memory::search_baudrate());
+	
+	button_set_search.on_select = [this, &nav](Button&) {
+		size_t search_def_index = options_search.selected_index();
+	};
+
+	button_save.on_select = [this,&nav](Button&) {
+		serial_format_t serial_format;
+		
+		persistent_memory::set_afsk_mark(field_mark.value());
+		persistent_memory::set_afsk_space(field_space.value());
+		
+		persistent_memory::set_search_baudrate(field_baudrate.value());
+		persistent_memory::set_search_repeat(field_repeat.value());
+		
+		serial_format.data_bits = sym_format.get_sym(0) + 6;
+		serial_format.parity = (parity_enum)sym_format.get_sym(1);
+		serial_format.stop_bits = sym_format.get_sym(2);
+		serial_format.bit_order = (order_enum)sym_format.get_sym(3);
+		
+		persistent_memory::set_serial_format(serial_format);
+		
+		nav.pop();
+	};
+}
+
+} /* namespace ui */
---
+++ firmware/application/apps/ui_searchsetup.hpp
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2014 Jared Boone, ShareBrained Technology, Inc.
+ * Copyright (C) 2016 Furrtek
+ *
+ * This file is part of PortaPack.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "serializer.hpp"
+
+#include "ui.hpp"
+#include "ui_widget.hpp"
+#include "ui_navigation.hpp"
+
+namespace ui {
+
+class SearchSetupView : public View {
+public:
+	SearchSetupView(NavigationView& nav);
+	
+	void focus() override;
+	
+	std::string title() const override { return "Search setup"; };
+
+private:
+	void update_freq(rf::Frequency f);
+	
+	Labels labels {
+		{ { 2 * 8, 11 * 8 }, "Baudrate:", Color::light_grey() },
+		{ { 2 * 8, 13 * 8 }, "Mark:      Hz", Color::light_grey() },
+		{ { 2 * 8, 15 * 8 }, "Space:     Hz", Color::light_grey() },
+		{ { 140, 15 * 8 }, "Repeat:", Color::light_grey() },
+		{ { 1 * 8, 6 * 8 }, "Search preset:", Color::light_grey() },
+		{ { 2 * 8, 22 * 8 }, "Serial format:", Color::light_grey() }
+	};
+
+	NumberField field_baudrate {
+		{ 11 * 8, 11 * 8 },
+		5,
+		{ 50, 9600 },
+		25,
+		' '
+	};
+
+	NumberField field_mark {
+		{ 8 * 8, 13 * 8 },
+		5,
+		{ 100, 15000 },
+		25,
+		' '
+	};
+	
+	NumberField field_space {
+		{ 8 * 8, 15 * 8 },
+		5,
+		{ 100, 15000 },
+		25,
+		' '
+	};
+	
+	NumberField field_repeat {
+		{ 204, 15 * 8 },
+		2,
+		{ 1, 99 },
+		1,
+		' '
+	};
+	
+	OptionsField options_search {
+		{ 15 * 8, 6 * 8 },
+		7,
+		{
+		}
+	};
+	
+	SymField sym_format {
+		{ 16 * 8, 22 * 8 },
+		4,
+		SymField::SYMFIELD_DEF
+	};
+	
+	Button button_set_search {
+		{ 23 * 8, 6 * 8 - 4, 6 * 8, 24 },
+		"SET"
+	};
+	
+	Button button_save {
+		{ 9 * 8, 250, 96, 40 },
+		"Save"
+	};
+
+
+};
+
+} /* namespace ui */
