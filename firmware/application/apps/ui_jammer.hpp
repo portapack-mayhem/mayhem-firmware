8859c3d80c06e3c0c7d9893cf71b0413886901cd
euquiq <31453004+euquiq@users.noreply.github.com>
2021-01-07 17:51:38 -0300

    JAMMER NOW INCLUDES TWO TIMERS
    Each driving the TX and COOLDOWN timers.

--- firmware/application/apps/ui_jammer.hpp
+++ firmware/application/apps/ui_jammer.hpp
@@ -57,73 +57,6 @@ private:
 		.foreground = Color::grey(),
 	};
 	
-	/*static constexpr jammer_range_t range_presets[] = {
-		// GSM900 Orange
-		{ true, 935000000, 945000000 },		// BW:10M
-		// GSM1800 Orange
-		{ true, 1808000000, 1832000000 },	// BW:24M
-		
-		// GSM900 SFR
-		{ true, 950000000, 960000000 },		// BW:10M
-		// GSM1800 SFR
-		{ true, 1832000000, 1853000000 },	// BW:21M
-		
-		// GSM900 Bouygues
-		{ true, 925000000, 935000000 },		// BW:10M
-		// GSM1800 Bouygues
-		{ true, 1858000000, 1880000000 },	// BW:22M
-		
-		// GSM900 Free
-		{ true, 945000000, 950000000 },		// BW:5M
-		
-		// GSM-R
-		{ true, 921000000, 925000000 },		// BW:4M
-		
-		// DECT
-		{ true, 1880000000, 1900000000 },	// BW: 20MHz
-		
-		// PMV AFSK
-		{ true, 162930000, 162970000 },		// BW: 40kHz
-		
-		// ISM 433
-		{ true, 433050000, 434790000 },		// Center: 433.92MHz BW: 0.2%
-		
-		// ISM 868
-		{ true, 868000000, 868200000 },		// Center: 868.2MHz BW: 40kHz
-		
-		// GPS L1
-		{ true, 1575420000 - 500000, 1575420000 + 500000 },		// BW: 1MHz
-		// GPS L2
-		{ true, 1227600000 - 1000000, 1227600000 + 1000000 },	// BW: 2MHz
-		
-		// WLAN 2.4G CH1
-		{ true, 2412000000 - 11000000, 2412000000 + 11000000},	// BW: 22MHz
-		// WLAN 2.4G CH2
-		{ true, 2417000000 - 11000000, 2417000000 + 11000000},	// BW: 22MHz
-		// WLAN 2.4G CH3
-		{ true, 2422000000 - 11000000, 2422000000 + 11000000},	// BW: 22MHz
-		// WLAN 2.4G CH4
-		{ true, 2427000000 - 11000000, 2427000000 + 11000000},	// BW: 22MHz
-		// WLAN 2.4G CH5
-		{ true, 2432000000 - 11000000, 2432000000 + 11000000},	// BW: 22MHz
-		// WLAN 2.4G CH6
-		{ true, 2437000000 - 11000000, 2437000000 + 11000000},	// BW: 22MHz
-		// WLAN 2.4G CH7
-		{ true, 2442000000 - 11000000, 2442000000 + 11000000},	// BW: 22MHz
-		// WLAN 2.4G CH8
-		{ true, 2447000000 - 11000000, 2447000000 + 11000000},	// BW: 22MHz
-		// WLAN 2.4G CH9
-		{ true, 2452000000 - 11000000, 2452000000 + 11000000},	// BW: 22MHz
-		// WLAN 2.4G CH10
-		{ true, 2457000000 - 11000000, 2457000000 + 11000000},	// BW: 22MHz
-		// WLAN 2.4G CH11
-		{ true, 2462000000 - 11000000, 2462000000 + 11000000},	// BW: 22MHz
-		// WLAN 2.4G CH12
-		{ true, 2467000000 - 11000000, 2467000000 + 11000000},	// BW: 22MHz
-		// WLAN 2.4G CH13
-		{ true, 2472000000 - 11000000, 2472000000 + 11000000},	// BW: 22MHz
-	};*/
-	
 	Labels labels {
 		{ { 2 * 8, 9 * 8 + 4 }, "Start", Color::light_grey() },
 		{ { 23 * 8, 9 * 8 + 4 }, "Stop", Color::light_grey() },
@@ -178,12 +111,16 @@ private:
 	NavigationView& nav_;
 	
 	void start_tx();
+	void on_timer();
 	void stop_tx();
 	void set_jammer_channel(uint32_t i, uint32_t width, uint64_t center, uint32_t duration);
 	void on_retune(const rf::Frequency freq, const uint32_t range);
 	
 	JammerChannel * jammer_channels = (JammerChannel*)shared_memory.bb_data.data;
 	bool jamming { false };
+	bool cooling { false };		//euquiq: Indicates jammer in cooldown
+	uint16_t seconds = 0;		//euquiq: seconds counter for toggling tx / cooldown
+	uint16_t mscounter = 0;		//euquiq: Internal ms counter for do_timer()
 	
 	static constexpr Style style_val {
 		.font = font::fixed_8x16,
@@ -209,13 +146,17 @@ private:
 	};
 	
 	Labels labels {
-		{ { 3 * 8, 12 * 16 }, "Type:", Color::light_grey() },
-		{ { 2 * 8, 13 * 16 }, "Speed:", Color::light_grey() },
-		{ { 5 * 8, 14 * 16 }, "Hop:", Color::light_grey() }
+		{ { 6 * 8, 12 * 16 }, "Type:", Color::light_grey() },
+		{ { 5 * 8, 13 * 16 }, "Speed:", Color::light_grey() },
+		{ { 7 * 8, 14 * 16 }, "Hop:", Color::light_grey() },
+		{ { 3 * 8, 15 * 16 }, "Time TX:", Color::light_grey() },
+		{ { 0 * 8, 16 * 16 }, "Time Pause:", Color::light_grey() },
+		{ { 16 * 8, 15 * 16 }, "Secs.", Color::light_grey() },
+		{ { 16 * 8, 16 * 16 }, "Secs.", Color::light_grey() }
 	};
 	
 	OptionsField options_type {
-		{ 9 * 8, 12 * 16 },
+		{ 12 * 8, 12 * 16 },
 		8,
 		{
 			{ "Rand FSK", 0 },
@@ -235,7 +176,7 @@ private:
 	};
 	
 	OptionsField options_speed {
-		{ 9 * 8, 13 * 16 },
+		{ 12 * 8, 13 * 16 },
 		6,
 		{
 			{ "10Hz  ", 10 },
@@ -247,7 +188,7 @@ private:
 	};
 	
 	OptionsField options_hop {
-		{ 9 * 8, 14 * 16 },
+		{ 12 * 8, 14 * 16 },
 		5,
 		{
 			{ "10ms ", 1 },
@@ -259,9 +200,25 @@ private:
 			{ "10s  ", 1000 }
 		}
 	};
+
+	NumberField field_timetx {
+		{ 12 * 8, 15 * 16 },
+		3,
+		{ 1, 180 },
+		1,
+		' ',
+	};
+
+	NumberField field_timepause {
+		{ 12 * 8, 16 * 16 },
+		2,
+		{ 1, 60 },
+		1,
+		' ',
+	};
 	
 	Button button_transmit {
-		{ 9 * 8, 16 * 16, 96, 48 },
+		{ 18 * 8, 35 * 8, 96, 24 },
 		"START"
 	};
 	
@@ -272,6 +229,14 @@ private:
 			this->on_retune(message->freq, message->range);
 		}
 	};
+
+		MessageHandlerRegistration message_handler_frame_sync {
+		Message::ID::DisplayFrameSync,
+		[this](const Message* const) {
+			this->on_timer();
+		}
+	};
 };
 
 } /* namespace ui */
+
