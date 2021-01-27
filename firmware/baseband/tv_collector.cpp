1483160df49a83026eee1f7a163e964148b902ad
GullCode <gullradriel@hotmail.com>
2021-01-27 15:45:04 +0100

    removed unused variables

--- firmware/baseband/tv_collector.cpp
+++ firmware/baseband/tv_collector.cpp
@@ -88,11 +88,9 @@ void TvCollector::post_message(const buffer_c16_t& data) {
 	// Called from baseband processing thread.
         float re, im;
 	float mag;
-        float max;
 	if( streaming && !channel_spectrum_request_update ) {
                 for(size_t i=0; i<256; i++) 
 		{
-			const auto s = data.p[i];
                         re = (float)(data.p[i].real());
 			im = (float)(data.p[i].imag());
 			mag = __builtin_sqrtf((re * re) + (im * im)) ;
