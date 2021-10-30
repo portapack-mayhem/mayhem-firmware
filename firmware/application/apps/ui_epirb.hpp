f34c1bd96b6704afc36964942f5a19cc545a1c57
Arne Luehrs <arne.luehrs@gmail.com>
2021-10-30 17:09:40 +0200

    New files for EPIRB RX and Decode
    Using copy of Sonde implementation to build EPIRB receiver/decoder

---
+++ firmware/application/apps/ui_epirb.hpp
@@ -0,0 +1,201 @@
+/*
+ * Copyright (C) 2015 Jared Boone, ShareBrained Technology, Inc.
+ * Copyright (C) 2017 Furrtek
+ *
+ * This file is part of PortaPack.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __UI_SONDE_H__
+#define __UI_SONDE_H__
+
+#include "ui_navigation.hpp"
+#include "ui_receiver.hpp"
+#include "ui_rssi.hpp"
+#include "ui_geomap.hpp"
+
+#include "event_m0.hpp"
+
+#include "log_file.hpp"
+
+#include "epirb_packet.hpp"
+
+#include <cstddef>
+#include <string>
+
+class EpirbLogger {
+public:
+	Optional<File::Error> append(const std::filesystem::path& filename) {
+		return log_file.append(filename);
+	}
+
+	void on_packet(const epirb::Packet& packet);
+
+private:
+	LogFile log_file { };
+};
+
+namespace ui {
+
+class EpirbView : public View {
+public:
+	static constexpr uint32_t sampling_rate = 2457600;
+	static constexpr uint32_t baseband_bandwidth = 1750000;
+
+	EpirbView(NavigationView& nav);
+	~EpirbView();
+
+	void focus() override;
+
+	std::string title() const override { return "Radioepirb RX"; };
+
+private:
+	std::unique_ptr<EpirbLogger> logger { };
+	uint32_t target_frequency_ { 402700000 };
+	bool logging { false };
+	bool use_crc { false };
+	bool beep { false };
+
+	epirb::GPS_data gps_info { };
+	epirb::temp_humid temp_humid_info { };
+	std::string epirb_id { };
+
+	// AudioOutput audio_output { };
+
+	Labels labels {
+		{ { 4 * 8, 2 * 16 }, "Type:", Color::light_grey() },
+		{ { 6 * 8, 3 * 16 }, "ID:", Color::light_grey() },
+		{ { 0 * 8, 4 * 16 }, "DateTime:", Color::light_grey() },
+
+		{ { 3 * 8, 5 * 16 }, "Vbatt:", Color::light_grey() },
+		{ { 3 * 8, 6 * 16 }, "Frame:", Color::light_grey() },
+		{ { 4 * 8, 7 * 16 }, "Temp:", Color::light_grey() },
+		{ { 0 * 8, 8 * 16 }, "Humidity:", Color::light_grey() }
+	};
+
+	FrequencyField field_frequency {
+		{ 0 * 8, 0 * 8 },
+	};
+
+	RFAmpField field_rf_amp {
+		{ 13 * 8, 0 * 16 }
+	};
+
+	LNAGainField field_lna {
+		{ 15 * 8, 0 * 16 }
+	};
+
+	VGAGainField field_vga {
+		{ 18 * 8, 0 * 16 }
+	};
+
+	RSSI rssi {
+		{ 21 * 8, 0, 6 * 8, 4 },
+	};
+
+	NumberField field_volume {
+		{ 28 * 8, 0 * 16 },
+		2,
+		{ 0, 99 },
+		1,
+		' ',
+	};
+
+
+	Checkbox check_beep {
+		{ 22 * 8, 6 * 16 },
+		3,
+		"Beep"
+	};
+
+	Checkbox check_log {
+		{ 22 * 8, 8 * 16 },
+		3,
+		"Log"
+	};
+
+	Checkbox check_crc {
+		{ 22 * 8, 10 * 16 },
+		3,
+		"CRC"
+	};
+
+	Text text_signature {
+		{ 9 * 8, 2 * 16, 10 * 8, 16 },
+		"..."
+	};
+
+	Text text_serial {
+		{ 9 * 8, 3 * 16, 11 * 8, 16 },
+		"..."
+	};
+
+	Text text_timestamp {
+		{ 9 * 8, 4 * 16, 11 * 8, 16 },
+		"..."
+	};
+
+	Text text_voltage {
+		{ 9 * 8, 5 * 16, 10 * 8, 16 },
+		"..."
+	};
+
+	Text text_frame {
+		{ 9 * 8, 6 * 16, 10 * 8, 16 },
+		"..."
+	};
+
+	Text text_temp {
+		{ 9 * 8, 7 * 16, 10 * 8, 16 },
+		"..."
+	};
+
+	Text text_humid {
+		{ 9 * 8, 8 * 16, 10 * 8, 16 },
+		"..."
+	};
+
+	GeoPos geopos {
+		{ 0, 12 * 16 },
+		GeoPos::alt_unit::METERS
+	};
+
+	Button button_see_map {
+		{ 8 * 8, 16 * 16, 14 * 8, 3 * 16 },
+		"See on map"
+	};
+
+	MessageHandlerRegistration message_handler_packet {
+		Message::ID::EpirbPacket,
+		[this](Message* const p) {
+			const auto message = static_cast<const EpirbPacketMessage*>(p);
+			const epirb::Packet packet { message->packet, message->type };
+			this->on_packet(packet);
+		}
+	};
+
+	void on_packet(const epirb::Packet& packet);
+	void on_headphone_volume_changed(int32_t v);
+
+	void set_target_frequency(const uint32_t new_value);
+
+	uint32_t tuning_frequency() const;
+};
+
+} /* namespace ui */
+
+#endif/*__UI_SONDE_H__*/
---
+++ firmware/common/epirb_packet.hpp
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2015 Jared Boone, ShareBrained Technology, Inc.
+ * Copyright (C) 2017 Furrtek
+ *
+ * This file is part of PortaPack.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __EPIRB_PACKET_H__
+#define __EPIRB_PACKET_H__
+
+#include <cstdint>
+#include <cstddef>
+
+#include "field_reader.hpp"
+#include "baseband_packet.hpp"
+#include "manchester.hpp"
+
+namespace epirb {
+
+	struct GPS_data {
+		uint32_t alt { 0 };
+		float lat { 0 };
+		float lon { 0 };
+	};
+
+	struct temp_humid {
+		float temp { 0 };
+		float humid { 0 };
+	};
+
+class Packet {
+public:
+	enum class Type : uint32_t {
+		Unknown = 0,
+		Meteomodem_unknown = 1,
+		Meteomodem_M10 = 2,
+		Meteomodem_M2K2 = 3,
+		Vaisala_RS41_SG = 4,
+	};
+
+	Packet(const baseband::Packet& packet, const Type type);
+
+	size_t length() const;
+
+	Timestamp received_at() const;
+
+	Type type() const;
+	std::string type_string() const;
+
+	std::string serial_number() const;
+	uint32_t battery_voltage() const;
+	GPS_data get_GPS_data() const;
+	uint32_t frame() const;
+	temp_humid get_temp_humid() const;
+
+	FormattedSymbols symbols_formatted() const;
+
+	bool crc_ok() const;
+
+private:
+	static constexpr uint8_t vaisala_mask[64] = {
+		0x96, 0x83, 0x3E, 0x51, 0xB1, 0x49, 0x08, 0x98,
+		0x32, 0x05, 0x59, 0x0E, 0xF9, 0x44, 0xC6, 0x26,
+		0x21, 0x60, 0xC2, 0xEA, 0x79, 0x5D, 0x6D, 0xA1,
+		0x54, 0x69, 0x47, 0x0C, 0xDC, 0xE8, 0x5C, 0xF1,
+		0xF7, 0x76, 0x82, 0x7F, 0x07, 0x99, 0xA2, 0x2C,
+		0x93, 0x7C, 0x30, 0x63, 0xF5, 0x10, 0x2E, 0x61,
+		0xD0, 0xBC, 0xB4, 0xB6, 0x06, 0xAA, 0xF4, 0x23,
+		0x78, 0x6E, 0x3B, 0xAE, 0xBF, 0x7B, 0x4C, 0xC1
+	};
+
+	GPS_data ecef_to_gps() const;
+
+	uint8_t vaisala_descramble(uint32_t pos) const;
+
+	const baseband::Packet packet_;
+	const BiphaseMDecoder decoder_;
+	const FieldReader<BiphaseMDecoder, BitRemapNone> reader_bi_m;
+	Type type_;
+
+	using packetReader = FieldReader<baseband::Packet, BitRemapByteReverse>; //baseband::Packet instead of BiphaseMDecoder
+
+	bool crc_ok_M10() const;
+	bool crc_ok_RS41() const;
+	bool crc16rs41(uint32_t field_start) const;
+};
+
+} /* namespace epirb */
+
+#endif/*__EPIRB_PACKET_H__*/
