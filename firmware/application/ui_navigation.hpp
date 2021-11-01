da380261b210160799ae4a2d06d418dedb2ca41f
eried <1091420+eried@users.noreply.github.com>
2021-11-01 11:49:42 +0100

    Version bump

--- firmware/application/ui_navigation.hpp
+++ firmware/application/ui_navigation.hpp
@@ -214,7 +214,7 @@ public:
 	InformationView(NavigationView& nav);
 	void refresh();	
 private:
-	static constexpr auto version_string = "v1.4.0";
+	static constexpr auto version_string = "v1.4.1";
 	NavigationView& nav_;
 
 	Rectangle backdrop {
