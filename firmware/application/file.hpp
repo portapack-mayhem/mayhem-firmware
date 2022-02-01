e00d91e1d247e9be13aa83e36cb4d6e2a8a27c02
notpike <e1000basetx@gmail.com>
2022-02-01 10:37:38 -0800

    Added return codes for delete_file() and rename_file() for file.hpp

--- firmware/application/file.hpp
+++ firmware/application/file.hpp
@@ -238,8 +238,8 @@ struct FATTimestamp {
 	uint16_t FAT_time;
 };
 
-void delete_file(const std::filesystem::path& file_path);
-void rename_file(const std::filesystem::path& file_path, const std::filesystem::path& new_name);
+uint32_t delete_file(const std::filesystem::path& file_path);
+uint32_t rename_file(const std::filesystem::path& file_path, const std::filesystem::path& new_name);
 FATTimestamp file_created_date(const std::filesystem::path& file_path);
 uint32_t make_new_directory(const std::filesystem::path& dir_path);
 
