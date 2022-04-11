24fbc4e449e9bbb48f86d60714ce27acd7b5e1c6
jLynx <admin@jlynx.net>
2022-04-12 08:24:18 +1200

    Refactored

--- firmware/application/portapack.cpp
+++ firmware/application/portapack.cpp
@@ -215,7 +215,7 @@ static int load_config(){
 	static Optional<int> config_value;
 	if(!config_value.is_valid()){
 		int8_t value = portapack::persistent_memory::config_cpld();
-		if(value == 0 && sd_card::status() == sd_card::Status::Mounted){
+		if((value <= 0 || value >= 4) && sd_card::status() == sd_card::Status::Mounted){
 			int data = read_file("/hardware/settings.txt");
 			if(data != -1) {
 				config_value = data;
@@ -227,30 +227,31 @@ static int load_config(){
 	return config_value.value();
 }
 
+
 static PortaPackModel portapack_model() {
 	static Optional<PortaPackModel> model;
 
 	if( !model.is_valid() ) {
 		const auto switches_state = get_switches_state();
 		if (switches_state[(size_t)ui::KeyEvent::Up]){
-			persistent_memory::set_config_cpld(1);
+			save_config(1);
 			model = PortaPackModel::R2_20170522;
 		}
 		else if (switches_state[(size_t)ui::KeyEvent::Down]){
-			persistent_memory::set_config_cpld(2);
+			save_config(2);
 			model = PortaPackModel::R1_20150901;
 		}
 		else if (switches_state[(size_t)ui::KeyEvent::Left]){
-			persistent_memory::set_config_cpld(3);
+			save_config(3);
 		}
 		else if (switches_state[(size_t)ui::KeyEvent::Select]){
-			persistent_memory::set_config_cpld(0);
+			save_config(0);
 		}
 		
 
-		if (portapack::persistent_memory::config_cpld() == 1) {
+		if (load_config() == 1) {
 			model = PortaPackModel::R2_20170522;
-		} else if (portapack::persistent_memory::config_cpld() == 2) {
+		} else if (load_config() == 2) {
 			model = PortaPackModel::R1_20150901;
 		} else {
 			if( audio_codec_wm8731.detected() ) {
@@ -276,28 +277,6 @@ static audio::Codec* portapack_audio_codec() {
 }
 
 static const portapack::cpld::Config& portapack_cpld_config() {
-	const auto switches_state = get_switches_state();
-	if (switches_state[(size_t)ui::KeyEvent::Up]){
-		save_config(1);
-		return portapack::cpld::rev_20170522::config;
-	}
-	if (switches_state[(size_t)ui::KeyEvent::Down]){
-		save_config(2);
-		return portapack::cpld::rev_20150901::config;
-	}
-	if (switches_state[(size_t)ui::KeyEvent::Left]){
-		save_config(3);
-	}
-	if (switches_state[(size_t)ui::KeyEvent::Select]){
-		save_config(0);
-	}
-	
-
-	if (load_config() == 1) {
-		return portapack::cpld::rev_20170522::config;
-	} else if (load_config() == 2) {
-		return portapack::cpld::rev_20150901::config;
-	}
 	return (portapack_model() == PortaPackModel::R2_20170522)
 			? portapack::cpld::rev_20170522::config
 			: portapack::cpld::rev_20150901::config;
@@ -486,7 +465,7 @@ bool init() {
 	sdcStart(&SDCD1, nullptr);
 	sd_card::poll_inserted();
 	
-	chThdSleepMilliseconds(1);
+	chThdSleepMilliseconds(10);
 
 	if( !portapack::cpld::update_if_necessary(portapack_cpld_config()) ) {
 		// If using a "2021/12 QFP100", press and hold the left button while booting. Should only need to do once.
@@ -495,6 +474,7 @@ bool init() {
 		 * But for some reason the persistent_memory check fails on some devices if we dont have the extra check in....
 		 * So dont ask me why that is, but we have to keep this redundant check in for the persistent_memory check to work.
 		 */
+		chThdSleepMilliseconds(10);
 		if (load_config() != 3){
 			shutdown_base();
 			return false;
