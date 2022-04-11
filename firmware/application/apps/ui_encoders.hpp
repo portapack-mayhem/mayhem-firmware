a1c1fbd60e58f4ec36fa918cc51f54cec0c51114
José Moreira <josemoreiravarzim@gmail.com>
2022-04-11 21:52:27 +0100

    wip: ook debruijn

--- firmware/application/apps/ui_encoders.hpp
+++ firmware/application/apps/ui_encoders.hpp
@@ -29,197 +29,198 @@
 
 using namespace encoders;
 
-namespace ui {
+namespace ui
+{
 
-class EncodersConfigView : public View {
-public:
-	EncodersConfigView(NavigationView& nav, Rect parent_rect);
-	
-	EncodersConfigView(const EncodersConfigView&) = delete;
-	EncodersConfigView(EncodersConfigView&&) = delete;
-	EncodersConfigView& operator=(const EncodersConfigView&) = delete;
-	EncodersConfigView& operator=(EncodersConfigView&&) = delete;
-	
-	void focus() override;
-	void on_show() override;
-	
-	uint8_t repeat_min();
-	uint32_t samples_per_bit();
-	uint32_t pause_symbols();
-	void generate_frame();
-	
-	std::string frame_fragments = "0";
+	class EncodersConfigView : public View
+	{
+	public:
+		EncodersConfigView(NavigationView &nav, Rect parent_rect);
 
-private:
-	//bool abort_scan = false;
-	//uint8_t scan_count;
-	//double scan_progress;
-	//unsigned int scan_index;
-	int16_t waveform_buffer[550];
-	const encoder_def_t * encoder_def { };
-	//uint8_t enc_type = 0;
+		EncodersConfigView(const EncodersConfigView &) = delete;
+		EncodersConfigView(EncodersConfigView &&) = delete;
+		EncodersConfigView &operator=(const EncodersConfigView &) = delete;
+		EncodersConfigView &operator=(EncodersConfigView &&) = delete;
 
-	void draw_waveform();
-	void on_bitfield();
-	void on_type_change(size_t index);
-	
-	Labels labels {
-		{ { 1 * 8, 0 }, "Type:", Color::light_grey() },
-		{ { 16 * 8, 0 }, "Clk:", Color::light_grey() },
-		{ { 24 * 8, 0 }, "kHz", Color::light_grey() },
-		{ { 14 * 8, 2 * 8 }, "Frame:", Color::light_grey() },
-		{ { 26 * 8, 2 * 8 }, "us", Color::light_grey() },
-		{ { 2 * 8, 4 * 8 }, "Symbols:", Color::light_grey() },
-		{ { 1 * 8, 11 * 8 }, "Waveform:", Color::light_grey() }
+		void focus() override;
+		void on_show() override;
+
+		uint8_t repeat_min();
+
+		uint32_t samples_per_bit();
+		uint16_t repeat_skip_bits_count();
+		uint16_t sin_carrier_step();
+		uint32_t pause_symbols();
+		void generate_frame(bool is_debruijn, uint32_t debruijn_bits);
+
+		std::string frame_fragments = "0";
+
+	private:
+		bool abort_scan = false;
+		uint8_t scan_count;
+		uint8_t scan_index;
+		double scan_progress;
+		int16_t waveform_buffer[550];
+		const encoder_def_t *encoder_def{};
+		uint8_t enc_type = 0;
+		uint8_t bits_per_packet; // Euquiq: the number of bits needed from de_bruijn, depends on the encoder's needs
+		char str[16];
+
+		uint8_t afsk_repeats;
+		de_bruijn debruijn_seq;
+
+		void draw_waveform();
+		void on_bitfield();
+		void on_type_change(size_t index);
+
+		Labels labels{
+			{{1 * 8, 0}, "Type:", Color::light_grey()},
+			{{16 * 8, 0}, "Clk:", Color::light_grey()},
+			{{24 * 8, 0}, "kHz", Color::light_grey()},
+			{{14 * 8, 2 * 8}, "Frame:", Color::light_grey()},
+			{{26 * 8, 2 * 8}, "us", Color::light_grey()},
+			{{2 * 8, 4 * 8}, "Symbols:", Color::light_grey()},
+			{{1 * 8, 11 * 8}, "Waveform:", Color::light_grey()}};
+
+		OptionsField options_enctype{// Options are loaded at runtime
+									 {6 * 8, 0},
+									 7,
+									 {}};
+
+		NumberField field_clk{
+			{21 * 8, 0},
+			3,
+			{1, 500},
+			1,
+			' '};
+
+		NumberField field_frameduration{
+			{21 * 8, 2 * 8},
+			5,
+			{300, 99999},
+			100,
+			' '};
+
+		SymField symfield_word{
+			{2 * 8, 6 * 8},
+			20,
+			SymField::SYMFIELD_DEF};
+
+		Text text_format{
+			{2 * 8, 8 * 8, 24 * 8, 16},
+			""};
+
+		Waveform waveform{
+			{0, 14 * 8, 240, 32},
+			waveform_buffer,
+			0,
+			0,
+			true,
+			Color::yellow()};
 	};
 
-	OptionsField options_enctype {		// Options are loaded at runtime
-		{ 6 * 8, 0 },
-		7,
+	class EncodersScanView : public View
+	{
+	public:
+		EncodersScanView(NavigationView &nav, Rect parent_rect);
+
+		void focus() override;
+
+	private:
+		Labels labels{
+			{{1 * 8, 1 * 8}, "Coming soon...", Color::light_grey()}};
+
+		// DEBUG
+		NumberField field_debug{
+			{1 * 8, 6 * 8},
+			2,
+			{3, 16},
+			1,
+			' '};
+
+		// DEBUG
+		Text text_debug{
+			{1 * 8, 8 * 8, 24 * 8, 16},
+			""};
+
+		// DEBUG
+		Text text_length{
+			{1 * 8, 10 * 8, 24 * 8, 16},
+			""};
+	};
+
+	class EncodersView : public View
+	{
+	public:
+		EncodersView(NavigationView &nav);
+		~EncodersView();
+
+		void focus() override;
+
+		std::string title() const override { return "OOK transmit"; };
+
+	private:
+		NavigationView &nav_;
+
+		enum tx_modes
 		{
-		}
-	};
+			IDLE = 0,
+			SINGLE,
+			SCAN
+		};
 
-	NumberField field_clk {
-		{ 21 * 8, 0 },
-		3,
-		{ 1, 500 },
-		1,
-		' '
-	};
+		tx_modes tx_mode = IDLE;
+		uint8_t repeat_index{0};
+		uint8_t repeat_min{0};
 
-	NumberField field_frameduration {
-		{ 21 * 8, 2 * 8 },
-		5,
-		{ 300, 99999 },
-		100,
-		' '
-	};
-	
-	SymField symfield_word {
-		{ 2 * 8, 6 * 8 },
-		20,
-		SymField::SYMFIELD_DEF
-	};
-	
-	Text text_format {
-		{ 2 * 8, 8 * 8, 24 * 8, 16 },
-		""
-	};
-	
-	Waveform waveform {
-		{ 0, 14 * 8, 240, 32 },
-		waveform_buffer,
-		0,
-		0,
-		true,
-		Color::yellow()
-	};
-};
+		void update_progress();
+		void start_tx(const bool scan);
+		void on_tx_progress(const uint32_t progress, const bool done);
 
+		/*const Style style_address {
+			.font = font::fixed_8x16,
+			.background = Color::black(),
+			.foreground = Color::red(),
+		};
+		const Style style_data {
+			.font = font::fixed_8x16,
+			.background = Color::black(),
+			.foreground = Color::blue(),
+		};*/
 
-class EncodersScanView : public View {
-public:
-	EncodersScanView(NavigationView& nav, Rect parent_rect);
-	
-	void focus() override;
+		Rect view_rect = {0, 4 * 8, 240, 168};
 
-private:
-	Labels labels {
-		{ { 1 * 8, 1 * 8 }, "Coming soon...", Color::light_grey() }
-	};
-	
-	// DEBUG
-	NumberField field_debug {
-		{ 1 * 8, 6 * 8 },
-		2,
-		{ 3, 16 },
-		1,
-		' '
-	};
-	
-	// DEBUG
-	Text text_debug {
-		{ 1 * 8, 8 * 8, 24 * 8, 16 },
-		""
-	};
-	
-	// DEBUG
-	Text text_length {
-		{ 1 * 8, 10 * 8, 24 * 8, 16 },
-		""
-	};
-};
+		EncodersConfigView view_config{nav_, view_rect};
+		EncodersScanView view_scan{nav_, view_rect};
 
-class EncodersView : public View {
-public:
-	EncodersView(NavigationView& nav);
-	~EncodersView();
-	
-	void focus() override;
-	
-	std::string title() const override { return "OOK transmit"; };
+		TabView tab_view{
+			{"Config", Color::cyan(), &view_config},
+			{"Scan", Color::green(), &view_scan},
+		};
 
-private:
-	NavigationView& nav_;
+		Text text_status{
+			{2 * 8, 13 * 16, 128, 16},
+			"Ready"};
 
-	enum tx_modes {
-		IDLE = 0,
-		SINGLE,
-		SCAN
-	};
-	
-	tx_modes tx_mode = IDLE;
-	uint8_t repeat_index { 0 };
-	uint8_t repeat_min { 0 };
-	
-	void update_progress();
-	void start_tx(const bool scan);
-	void on_tx_progress(const uint32_t progress, const bool done);
-	
-	/*const Style style_address {
-		.font = font::fixed_8x16,
-		.background = Color::black(),
-		.foreground = Color::red(),
-	};
-	const Style style_data {
-		.font = font::fixed_8x16,
-		.background = Color::black(),
-		.foreground = Color::blue(),
-	};*/
-	
-	Rect view_rect = { 0, 4 * 8, 240, 168 };
-	
-	EncodersConfigView view_config { nav_, view_rect };
-	EncodersScanView view_scan { nav_, view_rect };
-	
-	TabView tab_view {
-		{ "Config", Color::cyan(), &view_config },
-		{ "Scan", Color::green(), &view_scan },
-	};
+		Button button_scan{
+			{8 * 8, 11 * 16, 120, 28},
+			"DE BRUIJN TX"};
 
-	Text text_status {
-		{ 2 * 8, 13 * 16, 128, 16 },
-		"Ready"
+		ProgressBar progressbar{
+			{2 * 8, 13 * 16 + 20, 208, 16}};
+
+		TransmitterView tx_view{
+			16 * 16,
+			50000,
+			9};
+
+		MessageHandlerRegistration message_handler_tx_progress{
+			Message::ID::TXProgress,
+			[this](const Message *const p)
+			{
+				const auto message = *reinterpret_cast<const TXProgressMessage *>(p);
+				this->on_tx_progress(message.progress, message.done);
+			}};
 	};
-	
-	ProgressBar progressbar {
-		{ 2 * 8, 13 * 16 + 20, 208, 16 }
-	};
-	
-	TransmitterView tx_view {
-		16 * 16,
-		50000,
-		9
-	};
-	
-	MessageHandlerRegistration message_handler_tx_progress {
-		Message::ID::TXProgress,
-		[this](const Message* const p) {
-			const auto message = *reinterpret_cast<const TXProgressMessage*>(p);
-			this->on_tx_progress(message.progress, message.done);
-		}
-	};
-};
 
 } /* namespace ui */
--- firmware/application/protocols/encoders.hpp
+++ firmware/application/protocols/encoders.hpp
@@ -27,202 +27,315 @@
 #ifndef __ENCODERS_H__
 #define __ENCODERS_H__
 
-namespace encoders {
-	
-	#define ENC_TYPES_COUNT 14
-	#define OOK_SAMPLERATE	2280000U
-	
-	#define ENCODER_UM3750	8
-	
-	size_t make_bitstream(std::string& fragments);
-	void bitstream_append(size_t& bitstream_length, uint32_t bit_count, uint32_t bits);
+namespace encoders
+{
 
-	struct encoder_def_t {
-		char name[16];							// Encoder chip ref/name
-		char address_symbols[8];				// List of possible symbols like "01", "01F"...
-		char data_symbols[8];					// Same
-		uint16_t clk_per_symbol;				// Oscillator periods per symbol
-		uint16_t clk_per_fragment;				// Oscillator periods per symbol fragment (state)
-		char bit_format[4][20];					// List of fragments for each symbol in previous *_symbols list order
-		uint8_t word_length;					// Total # of symbols (not counting sync)
-		char word_format[32];					// A for Address, D for Data, S for sync
-		char sync[64];							// Like bit_format
-		uint32_t default_speed;					// Default encoder clk frequency (often set by shitty resistor)
-		uint8_t repeat_min;						// Minimum repeat count
-		uint16_t pause_symbols;					// Length of pause between repeats in symbols
+#define ENC_TYPES_COUNT 14
+#define OOK_SAMPLERATE 2280000U
+#define OOK_DEFAULT_STEP 8 // 70 kHz carrier frequency
+
+#define ENCODER_UM3750 8 // TODO: should this be 10?
+
+	size_t make_bitstream(std::string &fragments);
+	void bitstream_append(size_t &bitstream_length, uint32_t bit_count, uint32_t bits);
+
+	struct encoder_def_t
+	{
+		char name[16];			   // Encoder chip ref/name
+		char address_symbols[8];   // List of possible symbols like "01", "01F"...
+		char data_symbols[8];	   // Same
+		uint16_t clk_per_symbol;   // Oscillator periods per symbol
+		uint16_t clk_per_fragment; // Oscillator periods per symbol fragment (state)
+		char bit_format[4][20];	   // List of fragments for each symbol in previous *_symbols list order
+		uint8_t word_length;	   // Total # of symbols (not counting sync)
+		char word_format[32];	   // A for Address, D for Data, S for sync
+		char sync[64];			   // Like bit_format
+		uint32_t default_speed;	   // Default encoder clk frequency (often set by shitty resistor)
+		uint8_t repeat_min;		   // Minimum repeat count
+		uint16_t pause_symbols;	   // Length of pause between repeats in symbols
+		bool skip_repeat_bits;	   // Should we skip the sync/header bits once the first frame has been sent?
+		uint8_t sin_carrier_step;  // The sin table step for the carrier frequency, step = 256 / (2.28 MHz / fc), where 4 <= step < 256
 	};
 
 	// Warning ! If this is changed, make sure that ENCODER_UM3750 is still valid !
 	constexpr encoder_def_t encoder_defs[ENC_TYPES_COUNT] = {
+		// Test OOK Doorbell
+		{
+			"Doorbel",
+			"01",
+			"01",
+			228,
+			57,
+			{"1000", "1110"},
+			24,
+			"AAAAAAAAAAAAAAAAAAAAAAAA",
+			"",
+			141260,
+			32, // repeat=32
+			32,
+			false,
+			21 // fc = 164 kHz
+		},
+
+		// Test OOK Garage Door
+		{
+			"OH200DC",
+			"01",
+			"01",
+			920,
+			115,
+			{"10000000", "11110000"},
+			8,
+			"AAAAAAAA",
+			"",
+			285000,
+			8, // repeat=230, looks like 8 is still working
+			70,
+			false,
+			6 // fc = ~50 kHz
+		},
+
 		// PT2260-R2
 		{
 			"2260-R2",
-			"01F", "01",
-			1024, 128,
-			{ "10001000", "11101110", "10001110" },
-			12,	"AAAAAAAAAADDS",
+			"01F",
+			"01",
+			1024,
+			128,
+			{"10001000", "11101110", "10001110"},
+			12,
+			"AAAAAAAAAADDS",
 			"10000000000000000000000000000000",
-			150000,	2,
-			0
-		},
-		
+			150000,
+			2,
+			0,
+			false,
+			OOK_DEFAULT_STEP},
+
 		// PT2260-R4
 		{
 			"2260-R4",
-			"01F", "01",
-			1024, 128,
-			{ "10001000", "11101110", "10001110" },
-			12,	"AAAAAAAADDDDS",
+			"01F",
+			"01",
+			1024,
+			128,
+			{"10001000", "11101110", "10001110"},
+			12,
+			"AAAAAAAADDDDS",
 			"10000000000000000000000000000000",
-			150000,	2,
-			0
-		},
-		
+			150000,
+			2,
+			0,
+			false,
+			OOK_DEFAULT_STEP},
+
 		// PT2262
 		{
 			"2262   ",
-			"01F", "01F",
-			32, 4,
-			{ "10001000", "11101110", "10001110" },
-			12,	"AAAAAAAAAAAAS",
+			"01F",
+			"01F",
+			32,
+			4,
+			{"10001000", "11101110", "10001110"},
+			12,
+			"AAAAAAAAAAAAS",
 			"10000000000000000000000000000000",
-			20000,	4,
-			0
-		},
-		
+			20000,
+			4,
+			0,
+			false,
+			OOK_DEFAULT_STEP},
+
 		// 16-bit ?
 		{
 			"16-bit ",
-			"01", "01",
-			32, 8,
-			{ "1110", "1000" },		// Opposite ?
-			16,	"AAAAAAAAAAAAAAAAS",
+			"01",
+			"01",
+			32,
+			8,
+			{"1110", "1000"}, // Opposite ?
+			16,
+			"AAAAAAAAAAAAAAAAS",
 			"100000000000000000000",
-			25000,	50,
-			0	// ?
-		},
-		
+			25000,
+			50,
+			0, // ?
+			false,
+			OOK_DEFAULT_STEP},
+
 		// RT1527
 		{
 			"1527   ",
-			"01", "01",
-			128, 32,
-			{ "1000", "1110" },
-			24,	"SAAAAAAAAAAAAAAAAAAAADDDD",
+			"01",
+			"01",
+			128,
+			32,
+			{"1000", "1110"},
+			24,
+			"SAAAAAAAAAAAAAAAAAAAADDDD",
 			"10000000000000000000000000000000",
-			100000,	4,
-			10	// ?
-		},
-		
+			100000,
+			4,
+			10, // ?
+			false,
+			OOK_DEFAULT_STEP},
+
 		// HK526E
 		{
 			"526E   ",
-			"01", "01",
-			24, 8,
-			{ "110", "100" },
-			12,	"AAAAAAAAAAAA",
+			"01",
+			"01",
+			24,
+			8,
+			{"110", "100"},
+			12,
+			"AAAAAAAAAAAA",
 			"",
-			20000, 4,
-			10	// ?
-		},
-		
+			20000,
+			4,
+			10, // ?
+			false,
+			OOK_DEFAULT_STEP},
+
 		// HT12E
 		{
 			"12E    ",
-			"01", "01",
-			3, 1,
-			{ "011", "001" },
-			12,	"SAAAAAAAADDDD",
+			"01",
+			"01",
+			3,
+			1,
+			{"011", "001"},
+			12,
+			"SAAAAAAAADDDD",
 			"0000000000000000000000000000000000001",
-			3000, 4,
-			10	// ?
-		},
-			
+			3000,
+			4,
+			10, // ?
+			false,
+			OOK_DEFAULT_STEP},
+
 		// VD5026 13 bits ?
 		{
 			"5026   ",
-			"0123", "0123",
-			128, 8,
-			{ "1000000010000000", "1111111011111110", "1111111010000000", "1000000011111110" },
-			12,	"SAAAAAAAAAAAA",
-			"000000000000000000000000000000000000000000000001",		// ?
-			100000,	4,
-			10	// ?
-		},
-		
+			"0123",
+			"0123",
+			128,
+			8,
+			{"1000000010000000", "1111111011111110", "1111111010000000", "1000000011111110"},
+			12,
+			"SAAAAAAAAAAAA",
+			"000000000000000000000000000000000000000000000001", // ?
+			100000,
+			4,
+			10, // ?
+			false,
+			OOK_DEFAULT_STEP},
+
 		// UM3750
 		{
 			"UM3750 ",
-			"01", "01",
-			96, 32,
-			{ "011", "001" },
-			12,	"SAAAAAAAAAAAA",
+			"01",
+			"01",
+			96,
+			32,
+			{"011", "001"},
+			12,
+			"SAAAAAAAAAAAA",
 			"001",
-			100000,	4,
-			(3 * 12) - 6	// Compensates for pause delay bug in proc_ook
-		},
-		
+			100000,
+			4,
+			(3 * 12) - 6, // Compensates for pause delay bug in proc_ook
+			false,
+			OOK_DEFAULT_STEP},
+
 		// UM3758
 		{
 			"UM3758 ",
-			"01F", "01",
-			96, 16,
-			{ "011011", "001001", "011001" },
-			18,	"SAAAAAAAAAADDDDDDDD",
+			"01F",
+			"01",
+			96,
+			16,
+			{"011011", "001001", "011001"},
+			18,
+			"SAAAAAAAAAADDDDDDDD",
 			"1",
-			160000,	4,
-			10	// ?
-		},
-		
+			160000,
+			4,
+			10, // ?
+			false,
+			OOK_DEFAULT_STEP},
+
 		// BA5104
 		{
 			"BA5104 ",
-			"01", "01",
-			3072, 768,
-			{ "1000", "1110" },
-			9,	"SDDAAAAAAA",
+			"01",
+			"01",
+			3072,
+			768,
+			{"1000", "1110"},
+			9,
+			"SDDAAAAAAA",
 			"",
-			455000,	4,
-			10	// ?
-		},
-			
+			455000,
+			4,
+			10, // ?
+			false,
+			OOK_DEFAULT_STEP},
+
 		// MC145026
 		{
 			"145026 ",
-			"01F", "01",
-			16, 1,
-			{ "0111111101111111", "0100000001000000", "0111111101000000" },
-			9,	"SAAAAADDDD",
+			"01F",
+			"01",
+			16,
+			1,
+			{"0111111101111111", "0100000001000000", "0111111101000000"},
+			9,
+			"SAAAAADDDD",
 			"000000000000000000",
-			455000,	2,
-			2
-		},
-		
+			455000,
+			2,
+			2,
+			false,
+			OOK_DEFAULT_STEP},
+
 		// HT6*** TODO: Add individual variations
 		{
 			"HT6*** ",
-			"01F", "01",
-			198, 33,
-			{ "011011", "001001", "001011" },
-			18,	"SAAAAAAAAAAAADDDDDD",
+			"01F",
+			"01",
+			198,
+			33,
+			{"011011", "001001", "001011"},
+			18,
+			"SAAAAAAAAAAAADDDDDD",
 			"0000000000000000000000000000000000001011001011001",
-			80000,	3,
-			10	// ?
-		},
-		
+			80000,
+			3,
+			10, // ?
+			false,
+			OOK_DEFAULT_STEP},
+
 		// TC9148
 		{
 			"TC9148 ",
-			"01", "01",
-			48, 12,
-			{ "1000", "1110", },
-			12,	"AAAAAAAAAAAA",
+			"01",
+			"01",
+			48,
+			12,
+			{
+				"1000",
+				"1110",
+			},
+			12,
+			"AAAAAAAAAAAA",
 			"",
-			455000,	3,
-			10	// ?
-		}
-	};
+			455000,
+			3,
+			10, // ?
+			false,
+			OOK_DEFAULT_STEP};
 
 } /* namespace encoders */
 
-#endif/*__ENCODERS_H__*/
+#endif /*__ENCODERS_H__*/
