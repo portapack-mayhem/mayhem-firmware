56526a571031efddb79238e79bdcd1ca678c1157
GullCode <gullradriel@hotmail.com>
2023-03-22 14:38:00 +0100

    enabled 1s peak hold on rssi view

--- firmware/application/apps/ui_recon.cpp
+++ firmware/application/apps/ui_recon.cpp
@@ -890,6 +890,7 @@ namespace ui {
 
         
         rssi.set_focusable(true);
+        rssi.set_peak( true , 1000 );
         rssi.on_select = [this](RSSI&) {
             recon_thread->stop();
             nav_.pop();
