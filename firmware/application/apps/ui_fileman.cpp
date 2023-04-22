d8e9fad20564c0c7f46172bf3ebba39de9fd61ea
zxkmmOnHaseeWSL <zxkmm@hotmail.com>
2023-04-22 17:51:31 +0800

    fix indentation for pr 20230422-2

--- firmware/application/apps/ui_fileman.cpp
+++ firmware/application/apps/ui_fileman.cpp
@@ -356,7 +356,7 @@ FileManagerView::FileManagerView(
 		button_refactor.on_select = [this, &nav](Button&) {
 			name_buffer = entry_list[menu_view.highlighted_index()].entry_path.filename().string().substr(0, max_filename_length);
 			on_refactor(nav);
-        };
+		};
 
 		button_delete.on_select = [this, &nav](Button&) {
 			// Use display_modal ?
