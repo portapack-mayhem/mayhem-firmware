e8b8f0ca5c7d880456968ad31ac655e6d982acd1
Joel M <joel.martin.2104@gmail.com>
2023-04-30 12:06:37 +0200

    Disable inaccurate altitude for the moment

--- firmware/common/sonde_packet.cpp
+++ firmware/common/sonde_packet.cpp
@@ -113,7 +113,7 @@ GPS_data Packet::get_GPS_data() const
 	}
 	else if (type_ == Type::Meteomodem_M20)
 	{
-		result.alt = ((reader_bi_m.read(8 * 8, 32) / 100) - 48) / 250 ; //Conversion of altitude is actually a bit shifted, needs to be more accurate.
+		result.alt = 0; //((reader_bi_m.read(8 * 8, 32) / 100) - 48) / 250 ; //Disable innacurate altitude for the moment.
 		result.lat = reader_bi_m.read(28 * 8, 32) / 1000000.0 ; //https://raw.githubusercontent.com/projecthorus/radiosonde_auto_rx/master/demod/mod/m20mod.c
 		result.lon = reader_bi_m.read(32 * 8, 32) / 1000000.0 ; 
 	}
