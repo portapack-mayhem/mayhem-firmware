bb992ea955b5e2067b7e1ee8c4b6d76c1cf86963
Mark Thompson <129641948+NotherNgineer@users.noreply.github.com>
2023-05-15 16:18:56 -0500

    Scanner enhancements (issue #986)
    The current Scanner app does not allow scrolling through the frequency list using the rotary encoder. I'm changing the PAUSE/RESUME button to allow manual scrolling through frequencies (via the encoder) when focus is on this button. Frequency scrolling will be supported regardless whether or not in PAUSE mode (for forcing a skip to the next frequency before the Wait timer elapses).
    
    The current Wait timer forces a Skip to the next frequency when the timer elapses and a value of 0 means "never lock to a frequency" (which is not useful); I've changed the meaning of "0" to lock on the frequency forever (or until the signal is lost).
    
    Add a second configurable timer setting to maintain lock on a frequency for a specified number of seconds following signal loss. This makes it easier to listen to a conversation on HAM radio (someone will often reply a second or two later) without having to press PAUSE.
    
    I propose to call these two timer fields "Wsa" (wait while signal active) and "Wsl" (wait after signal lost), versus the current "WAIT" field which is more ambiguous.
    
    I propose that the above "Timer" settings will be in 1-second units. (The RECON app uses 1-ms units for two similar timers but I don't think it's necessary nor is there room on the screen)
    
    When a Freq file with a "Range" is read using the "LOAD" button, I propose to populate the Manual Start/End Search Range fields, versus trying to add up to 115 individual frequencies to the Freq List in memory (often overflowing this list meaning the whole range doesn't get searched).
    
    I propose to support Freq files which contain both Range and Single frequencies (but only a single Range in the file due to the change above). Single frequencies will be added to the Freq List, and Range will be loaded into the Start/End Search fields. Note that the current "ADD FQ" button already adds Single frequencies to a file with a Range in it.
    
    When loading from a Freq file, the default Modulation and Step and BW values shall be picked up from the file, if specified, versus leaving the settings on "AM, 9kHz step, 16kHz bw". CAVEAT: At this time I am not (yet) planning to support a separate Modulation type for each frequency in the in-memory Freq List; the first valid value found is used.
    
    Support all BW and Step values in FreqMan (and use the table from FreqMan versus having separate tables in the Scanner code).
    
    Change the "Manual Scan" button to toggle between "SCAN Freq List" & "Sequential Range SEARCH" modes without having to re-LOAD the Freq file. Keep Freq List in memory unless purposely cleared, and allow adding to this in-memory list while performing the Sequential Range Search.
    
    Add a CLEAR button to clear the Freq List in memory without having to load an empty Freq file.
    
    Fixed "FREQ DEL" button to handle removal of frequencies above 4gHz (was incorrectly passing freq in a U32).
    
    Support SQUELCH when scanning is paused, to reduce unnecessary white noise.
    
    Fixed unintentional FWD/REV direction change when switching between scanning Frequency List and performing a Range Search.
    
    Display "REVERSE" or "FORWARD" on the "FWD<>REV" button, indicating which mode will become active if the button is pressed.

--- firmware/application/apps/ui_scanner.hpp
+++ firmware/application/apps/ui_scanner.hpp
@@ -33,20 +33,16 @@
 #include "string_format.hpp"
 #include "file.hpp"
 
-
-#define MAX_FREQ_LOCK 10 		//50ms cycles scanner locks into freq when signal detected, to verify signal is not spureous
+#define SCANNER_SLEEP_MS			50	//ms that Scanner Thread sleeps per loop
+#define STATISTICS_UPDATES_PER_SEC	10
+#define MAX_FREQ_LOCK				10 	//# of 50ms cycles scanner locks into freq when signal detected, to verify signal is not spureous
 
 namespace ui {
-
-enum modulation_type { AM = 0,WFM,NFM };
-	
-string const mod_name[3] = {"AM", "WFM", "NFM"};
-size_t const mod_step[3] = {9000, 100000, 12500 };
 
 class ScannerThread {
 public:
 	ScannerThread(std::vector<rf::Frequency> frequency_list);
-	ScannerThread(const jammer::jammer_range_t& frequency_range, size_t def_step);
+	ScannerThread(const jammer::jammer_range_t& frequency_range, size_t def_step_hz);
 	~ScannerThread();
 
 	void set_scanning(const bool v);
@@ -55,9 +51,9 @@ public:
 	void set_freq_lock(const uint32_t v);
 	uint32_t is_freq_lock();
 
-	void set_freq_del(const uint32_t v);
-
-	void change_scanning_direction();
+	void set_freq_del(const rf::Frequency v);
+	void set_index_stepper(const int32_t v);
+	void set_scanning_direction(bool fwd);
 
 	void stop();
 
@@ -69,13 +65,16 @@ public:
 private:
 	std::vector<rf::Frequency> frequency_list_ { };
 	jammer::jammer_range_t frequency_range_ {false, 0, 0};
-	size_t	def_step_ { 0 };
+	size_t	def_step_hz_ { 0 };
 	Thread* thread { nullptr };
 	
 	bool _scanning { true };
-	bool _fwd { true };
+	bool _manual_search { false };
 	uint32_t _freq_lock { 0 };
-	uint32_t _freq_del { 0 };
+	rf::Frequency _freq_del { 0 };
+	uint32_t _freq_idx { 0 };
+	int32_t _stepper { 1 };
+	int32_t _index_stepper { 0 };
 	static msg_t static_fn(void* arg);
 	void run();
 	void create_thread();
@@ -122,33 +121,50 @@ private:
 	NavigationView& nav_;
 
 	void start_scan_thread();
-	void start_scan_thread(const jammer::jammer_range_t& frequency_range, size_t def_step);
-	size_t change_mode(uint8_t mod_type);
-	void show_max();
+	void change_mode(freqman_index_t mod_type);
+	void show_max_index();
 	void scan_pause();
 	void scan_resume();
 	void user_resume();
 	void frequency_file_load(std::string file_name, bool stop_all_before = false);
-
+	void bigdisplay_update(int32_t);
+	void update_squelch_while_paused(int32_t max_db);
 	void on_statistics_update(const ChannelStatistics& statistics);
 	void on_headphone_volume_changed(int32_t v);
 	void handle_retune(int64_t freq, uint32_t freq_idx);
 
 	jammer::jammer_range_t frequency_range { false, 0, 0 };  //perfect for manual scan task too...
 	int32_t squelch { 0 };
-	uint32_t timer { 0 };
-	uint32_t wait { 0 };
-	size_t	def_step { 0 };
+	uint32_t browse_timer { 0 };
+	uint32_t lock_timer { 0 };
+	uint32_t color_timer { 0 };
+	int32_t bigdisplay_current_color { -2 };
+	rf::Frequency bigdisplay_current_frequency { 0 };
+	uint32_t browse_wait { 0 };
+	uint32_t lock_wait { 0 };
 	freqman_db database { };
 	std::string loaded_file_name;
 	uint32_t current_index { 0 };
 	rf::Frequency current_frequency { 0 };
 	bool userpause { false };
+	bool manual_search { false };
+	bool fwd { true };						//to preserve direction setting even if scan_thread restarted
+
+	enum bigdisplay_color_type {
+		BDC_GREY,
+		BDC_YELLOW,
+		BDC_GREEN,
+		BDC_RED
+	 };
 	
+	std::string desc_freq_range_search = "SEARCHING...";
+	std::string desc_freq_list_scan    = "";
+
 	Labels labels {
 		{ { 0 * 8, 0 * 16 }, "LNA:   VGA:   AMP:  VOL:", Color::light_grey() },
-		{ { 0 * 8, 1* 16 }, "BW:       SQLCH:   db WAIT:", Color::light_grey() },
-		{ { 3 * 8, 10 * 16 }, "START        END     MANUAL", Color::light_grey() },
+		{ { 0 * 8, 1 * 16 }, "BW:       SQ:    Wsa:   Wsl:", Color::light_grey() },
+		{ { 0 * 8, 10 * 16 }, "SRCH START  SEARCH END  SWITCH", Color::light_grey() },
+
 		{ { 0 * 8, (26 * 8) + 4 }, "MODE:", Color::light_grey() },
 		{ { 11 * 8, (26 * 8) + 4 }, "STEP:", Color::light_grey() },
 	};
@@ -175,20 +191,28 @@ private:
 
 	OptionsField field_bw {
 		{ 3 * 8, 1 * 16 },
-		4,
+		6,
 		{ }
 	};		
 
 	NumberField field_squelch {
-		{ 18 * 8, 1 * 16 },
+		{ 13 * 8, 1 * 16 },
 		3,
  		{ -90, 20 },
 		1,
 		' ',
 	};
 
-	NumberField field_wait {
-		{ 27 * 8, 1 * 16 },
+	NumberField field_browse_wait {	//Signal-Active wait timer - time to wait before moving on even when signal locked
+		{ 21 * 8, 1 * 16 },
+		2,
+		{ 0, 99 },
+		1,
+		' ',
+	};
+
+	NumberField field_lock_wait {	//Signal-Lost wait timer - time to wait before moving on after losing signal lock
+		{ 28 * 8, 1 * 16 },
 		2,
 		{ 0, 99 },
 		1,
@@ -199,16 +223,16 @@ private:
 		{ 0 * 16, 2 * 16, 15 * 16, 8 },
 	}; 
 
-	Text text_cycle {
+	Text text_current_index {
 		{ 0, 3 * 16, 3 * 8, 16 },  
 	};
 
-	Text text_max {
+	Text text_max_index {
 		{ 4 * 8, 3 * 16, 18 * 8, 16 },  
 	};
 	
-	Text desc_cycle {
-		{0, 4 * 16, 240, 16 },	   
+	Text desc_current_index {
+		{0, 4 * 16, 240 - 6 * 8, 16 },
 	};
 
 	BigFrequency big_display {		//Show frequency in glamour
@@ -226,46 +250,31 @@ private:
 		""
 	};
 
-	Button button_manual_scan {
+	Button button_manual_search {
 		{ 24 * 8, 11 * 16, 6 * 8, 28 },
-		"SCAN"
+		""
 	};
 
 	OptionsField field_mode {
 		{ 5 * 8, (26 * 8) + 4 },
 		6,
-		{
-			{ " AM  ", 0 },
-			{ " WFM ", 1 },
-			{ " NFM ", 2 },
-		}
+		{ }		//Text strings get filled by freqman_set_modulation_option()
 	};
 
-	OptionsField step_mode {
+	OptionsField field_step {
 		{ 17 * 8, (26 * 8) + 4 },
 		12,
-		{
-			{ "5kHz (SA AM)", 	5000 },
-			{ "9kHz (EU AM)", 	9000 },
-			{ "10kHz(US AM)", 	10000 },
-			{ "50kHz (FM1)", 	50000 },
-			{ "100kHz(FM2)", 	100000 },
-			{ "6.25kHz(NFM)",	6250 },
-			{ "12.5kHz(NFM)",	12500 },
-			{ "25kHz (N1)",		25000 },
-			{ "250kHz (N2)",	250000 },
-			{ "8.33kHz(AIR)",	8330 }
-		}
+		{ }		//Text strings get filled by freqman_set_step_option()
 	};
 
-	Button button_pause {
+	ButtonWithEncoder button_pause {
 		{ 0, (15 * 16) - 4, 72, 28 },
-		"PAUSE"
+		"<PAUSE>"
 	};
 
 	Button button_dir {
 		{ 0,  (35 * 8) - 4, 72, 28 },
-		"FW><RV"
+		"REVERSE"
 	};
 
 	Button button_audio_app {
@@ -284,8 +293,13 @@ private:
 	};
 
 	Button button_load {
-		{ 24 * 8, 3 * 16 - 8, 6 * 8, 22 },
-		"Load"
+		{ 24 * 8, 3 * 16 - 10, 6 * 8, 22 },
+		"LOAD"
+	};
+
+	Button button_clear {
+		{ 24 * 8, 4 * 16, 6 * 8, 22 },
+		"MCLR"
 	};
 
 	Button button_remove {
