e60ff49087885dd1ff28162bd5a6406887fe95b4
Mark Thompson <129641948+NotherNgineer@users.noreply.github.com>
2023-05-23 00:15:11 -0500

    Support "repeat" when holding a direction button

--- firmware/application/hw/debounce.cpp
+++ firmware/application/hw/debounce.cpp
@@ -27,11 +27,28 @@
 bool Debounce::feed(const uint8_t bit) {
     history_ = (history_ << 1) | (bit & 1);
 
+    // "Repeat" handling - simulated button release
+    if (repeat_ctr_) {
+        // Make sure the button is still being held continuously
+        if (history_ == 0xFF) {
+            // Simulate button press every REPEAT_SUBSEQUENT_DELAY ticks
+            if (--repeat_ctr_ == 0) {
+                state_ = !state_;
+                repeat_ctr_ = REPEAT_SUBSEQUENT_DELAY / 2;
+                return true;
+            }
+        } else {
+            // It's a real button release; stop simulating
+            repeat_ctr_ = 0;
+        }   
+    }
+
     if (state_ == 0) {
         // Previous button state was 0 (released);
         // Has button been held for DEBOUNCE_COUNT ticks?
         if ((history_ & DEBOUNCE_MASK) == DEBOUNCE_MASK) {
             state_ = 1;
+            held_time_ = 0;
             return true;
         }
     } else {
@@ -40,6 +57,22 @@ bool Debounce::feed(const uint8_t bit) {
         if ((history_ & DEBOUNCE_MASK) == 0) {
             state_ = 0;
             return true;
+        }
+
+        // Repeat support is limited to the 4 directional buttons
+        if (repeat_enabled_) {
+            // Has button been held continuously for DEBOUNCE_REPEAT_DELAY?
+            if (history_ == 0xFF) {
+                if (++held_time_ == REPEAT_INITIAL_DELAY) {
+                    // Delay reached; trigger repeat code on NEXT tick
+                    repeat_ctr_ = 1;
+                    held_time_ = 0;
+                    return false;
+                }
+            } else {
+                // Button not continuously pressed; reset counter
+                held_time_ = 0;
+            }
         }
     }
     return false;
