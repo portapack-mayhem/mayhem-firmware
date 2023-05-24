bbf5cc6185fb52d4fe8043ea18f9f89b80259332
Mark Thompson <129641948+NotherNgineer@users.noreply.github.com>
2023-05-23 21:09:11 -0500

    Simpler support for multiple levels of encoder sensitivity, for issue #965
    Removed the convoluted code :-) and instead just using a 2-dimensional array to choose which transition map to use.  For now I only have 2 (vs 3) levels enabled as well, to save code space and because high-sensitivity is very touchy.

--- firmware/application/hw/encoder.cpp
+++ firmware/application/hw/encoder.cpp
@@ -22,7 +22,7 @@
 #include "encoder.hpp"
 #include "utility.hpp"
 
-// Now supporting 3 levels of rotary encoder dial sensitivity
+// Now supporting multiple levels of rotary encoder dial sensitivity
 //
 // Portapack H2 normally has a 30-step encoder, meaning one step (pulse) every
 // 12 degrees of rotation.
@@ -30,27 +30,61 @@
 // For each encoder "pulse" there are 4 state transitions, and we can choose
 // between looking at all of them (high sensitivity), half of them (medium/default),
 // or one quarter of them (low sensitivity).
-//
-// Each non-zero nibble in the transition_map represents an encoder state transition
-// that we interpret as either a 1 or -1.
-// Clockwise state transitions are:  00b->10b, 10b->11b, 11b->01b, 01b->00b.
-//                             i.e.  2, B, D, 4
-// Counter-clockwise transitions are: 00b->01b, 01b->11b, 11b->10b, 10b->00b.
-//                             i.e.  1, 7, E, 8
-//  e.g. 0x0B = 1011b means input transition from 10b->11b, which means clockwise +1.
-//       Notice that B is not in transition_map[0] so this transition is ignored when
-//       in "low sensitivity" mode.
-//
-// Four no-op states (no change) are 00b->00b, 01b->01b, 10b->10b, and 11b->11b,
-// (AKA 0, 5, A, and F), which are of course ignored since no rotation occurred.
-// Four additional non-zero values may be seen when the encoder dial is turned
-// rapidly and intermediate states are skipped 00b->11b, 01b->10b, 10b->01b, and 11b->00b
-// (AKA 3, 6, 9, or C); these are also (currently) ignored.
-//
-static const uint32_t transition_map_[NUM_DIAL_SENSITIVITY] = {
-    0x00070004,  // low sensitivity
-    0x008700B4,  // medium sensitivity
-    0xE187D2B4   // high sensitivity
+static const int8_t transition_map[][16] = {
+    { // low sensitivity
+        0,   // 0000: noop
+        0,   // 0001: start
+        0,   // 0010: start
+        0,   // 0011: rate
+        1,   // 0100: end
+        0,   // 0101: noop
+        0,   // 0110: rate
+        0,   // 0111: end
+        -1,  // 1000: end
+        0,   // 1001: rate
+        0,   // 1010: noop
+        0,   // 1011: end
+        0,   // 1100: rate
+        0,   // 1101: start
+        0,   // 1110: start
+        0,   // 1111: noop
+    },
+    {  // medium sensitivity
+        0,   // 0000: noop
+        0,   // 0001: start
+        0,   // 0010: start
+        0,   // 0011: rate
+        1,   // 0100: end
+        0,   // 0101: noop
+        0,   // 0110: rate
+        -1,  // 0111: end
+        -1,  // 1000: end
+        0,   // 1001: rate
+        0,   // 1010: noop
+        1,   // 1011: end
+        0,   // 1100: rate
+        0,   // 1101: start
+        0,   // 1110: start
+        0,   // 1111: noop
+    },
+    // {   // high sensitivity - disabled for now (unnecessary/touchy)
+    //     0,   // 0000: noop
+    //     -1,  // 0001: start
+    //     1,   // 0010: start
+    //     0,   // 0011: rate
+    //     1,   // 0100: end
+    //     0,   // 0101: noop
+    //     0,   // 0110: rate
+    //     -1,  // 0111: end
+    //     -1,  // 1000: end
+    //     0,   // 1001: rate
+    //     0,   // 1010: noop
+    //     1,   // 1011: end
+    //     0,   // 1100: rate
+    //     1,   // 1101: start
+    //     -1,  // 1110: start
+    //     0,   // 1111: noop
+    // }
 };
 
 int_fast8_t Encoder::update(
@@ -61,14 +95,5 @@ int_fast8_t Encoder::update(
     state <<= 1;
     state |= phase_1;
 
-    uint32_t tmap = transition_map_[sensitivity_];
-    do {
-        if ((tmap & 0x0F) == (state & 0x0F))
-            return 1;
-        if (((tmap >> 16) & 0x0F) == (state & 0x0F))
-            return -1;
-        tmap >>= 4;
-    } while ((tmap >> 16) != 0);
-
-    return 0;
+    return transition_map[sensitivity_][state & 0xf];
 }
