26a2146dcecafdd0c71d00cd430db61dd9a47b75
GullCode <gullradriel@hotmail.com>
2023-05-26 15:07:49 +0200

    replaced a hard coded value and reduced buf size

--- firmware/application/freqman.cpp
+++ firmware/application/freqman.cpp
@@ -114,7 +114,8 @@ bool load_freqman_file_ex(std::string& file_stem, freqman_db& db, bool load_freq
     char* line_end;
     std::string description;
     rf::Frequency frequency_a, frequency_b;
-    char file_data[257];
+    const int read_buf_size = 128;
+    char file_data[read_buf_size + 1];
     freqman_entry_type type;
     freqman_index_t modulation = 0;
     freqman_index_t bandwidth = 0;
@@ -128,21 +129,21 @@ bool load_freqman_file_ex(std::string& file_stem, freqman_db& db, bool load_freq
         return false;
 
     while (1) {
-        // Read a 256 bytes block from file
+        // Read a read_buf_size bytes block from file
         freqman_file.seek(file_position);
 
-        memset(file_data, 0, 257);
-        auto read_size = freqman_file.read(file_data, 256);
+        memset(file_data, 0, read_buf_size + 1);
+        auto read_size = freqman_file.read(file_data, read_buf_size);
         if (read_size.is_error())
             return false;  // Read error
 
-        file_position += 256;
+        file_position += read_buf_size;
 
         // Reset line_start to beginning of buffer
         line_start = file_data;
 
         // If EOF reached, insert 0x0A after, in case the last line doesn't have a C/R
-        if (read_size.value() < 256)
+        if (read_size.value() < read_buf_size)
             *(line_start + read_size.value()) = 0x0A;
 
         // Look for complete lines in buffer
@@ -222,8 +223,7 @@ bool load_freqman_file_ex(std::string& file_stem, freqman_db& db, bool load_freq
                 pos += 2;
                 length = std::min(strcspn(pos, ",\x0A"), (size_t)FREQMAN_DESC_MAX_LEN);
                 description = string(pos, length);
-            } else
-                description = "-";
+            }
             if ((type == SINGLE && load_freqs) || (type == RANGE && load_ranges) || (type == HAMRADIO && load_hamradios)) {
                 db.push_back({frequency_a, frequency_b, description, type, modulation, bandwidth, step, tone});
                 n++;
@@ -231,14 +231,14 @@ bool load_freqman_file_ex(std::string& file_stem, freqman_db& db, bool load_freq
             }
 
             line_start = line_end + 1;
-            if (line_start - file_data >= 256) break;
+            if (line_start - file_data >= read_buf_size) break;
         }
 
-        if (read_size.value() != 256)
+        if (read_size.value() != read_buf_size)
             break;  // End of file
 
         // Restart at beginning of last incomplete line
-        file_position -= (file_data + 256 - line_start);
+        file_position -= (file_data + read_buf_size - line_start);
     }
 
     /* populate implicitly specified modulation / bandwidth */
--- firmware/application/freqman.hpp
+++ firmware/application/freqman.hpp
@@ -32,10 +32,10 @@
 #include "string_format.hpp"
 #include "ui_widget.hpp"
 
-#define FREQMAN_DESC_MAX_LEN 24         // This is the number of characters that can be drawn in front of "R: TEXT..." before taking a full screen line
-#define FREQMAN_MAX_PER_FILE 115        // Maximum of entries we can read. This is a hardware limit
-                                        // It was tested and lowered to leave a bit of space to the caller
-#define FREQMAN_MAX_PER_FILE_STR "115"  // STRING OF FREQMAN_MAX_PER_FILE
+#define FREQMAN_DESC_MAX_LEN 24        // This is the number of characters that can be drawn in front of "R: TEXT..." before taking a full screen line
+#define FREQMAN_MAX_PER_FILE 60        // Maximum of entries we can read. This is a hardware limit
+                                       // It was tested and lowered to leave a bit of space to the caller
+#define FREQMAN_MAX_PER_FILE_STR "60"  // STRING OF FREQMAN_MAX_PER_FILE
 
 using namespace ui;
 using namespace std;
