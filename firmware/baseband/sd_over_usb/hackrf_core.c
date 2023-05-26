35b381ae77819e7b592c42a2ea37fd9cba430674
E.T <tamas@eisenberger.hu>
2023-05-27 01:06:05 +0200

    Fix SD over USB (#1081)
    Remove clkin_detect_init() from cpu_clock_init() mirroring upstream change from the hackrf repo
    
    Co-authored-by: Eisenberger Tamas <e.tamas@iwstudio.hu>

--- firmware/baseband/sd_over_usb/hackrf_core.c
+++ firmware/baseband/sd_over_usb/hackrf_core.c
@@ -807,10 +807,6 @@ void cpu_clock_init(void) {
     // CCU2_CLK_APLL_CFG = 0;
     // CCU2_CLK_SDIO_CFG = 0;
 #endif
-
-    if (detected_platform() == BOARD_ID_HACKRF1_R9) {
-        clkin_detect_init();
-    }
 }
 
 clock_source_t activate_best_clock_source(void) {
