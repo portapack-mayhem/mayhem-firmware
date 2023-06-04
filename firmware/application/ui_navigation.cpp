f02646bc088f424f6c677c77739cd0068de1aa13
GullCode <gullradriel@hotmail.com>
2023-06-04 16:15:33 +0200

    moved pmem setting restore before top bar is loaded

--- firmware/application/ui_navigation.cpp
+++ firmware/application/ui_navigation.cpp
@@ -126,6 +126,13 @@ SystemStatusView::SystemStatusView(
         &sd_card_status_view,
     });
 
+    File pmem_flag_file_handle;
+    std::string pmem_flag_file = "/SETTINGS/PMEM_FILEFLAG";
+    auto result = pmem_flag_file_handle.open(pmem_flag_file);
+    if (!result.is_valid()) {
+        portapack::persistent_memory::load_persistent_settings_from_file("SETTINGS/pmem_settings");
+    }
+
     if (portapack::persistent_memory::config_speaker())
         button_speaker.hidden(false);
     else
@@ -691,13 +698,6 @@ SystemView::SystemView(
         } else {*/
 
     navigation_view.push<SystemMenuView>();
-
-    File pmem_flag_file_handle;
-    std::string pmem_flag_file = "/SETTINGS/PMEM_FILEFLAG";
-    auto result = pmem_flag_file_handle.open(pmem_flag_file);
-    if (!result.is_valid()) {
-        portapack::persistent_memory::load_persistent_settings_from_file("SETTINGS/pmem_settings");
-    }
 
     if (portapack::persistent_memory::config_splash()) {
         navigation_view.push<BMPView>();
