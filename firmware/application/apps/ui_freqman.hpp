dbfb5f42d010996fa75e20f87fdf8b0436987e24
GullCode <gullradriel@hotmail.com>
2023-06-07 13:43:29 +0200

    Revert "fix for save deleting files"
    This reverts commit dfeed1e2021632e0583aef12f4db5533f7d07774.

--- firmware/application/apps/ui_freqman.hpp
+++ firmware/application/apps/ui_freqman.hpp
@@ -30,8 +30,6 @@
 #include "freqman.hpp"
 #include "ui_freqlist.hpp"
 
-static int32_t current_category_id = 0;
-
 namespace ui {
 
 class FreqManBaseView : public View {
@@ -48,6 +46,7 @@ class FreqManBaseView : public View {
     NavigationView& nav_;
     freqman_error error_{NO_ERROR};
     options_t categories{};
+    std::function<void(int32_t category_id)> on_change_category{nullptr};
     std::function<void(void)> on_select_frequency{nullptr};
     std::function<void(bool)> on_refresh_widgets{nullptr};
 
