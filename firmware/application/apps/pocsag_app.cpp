0ddd4ef69cb0db1634ea314d6645c18d221b8c2c
Mark Thompson <129641948+NotherNgineer@users.noreply.github.com>
2023-06-14 09:38:28 -0500

    Default POCSAG freq when no App Settings

--- firmware/application/apps/pocsag_app.cpp
+++ firmware/application/apps/pocsag_app.cpp
@@ -69,6 +69,9 @@ POCSAGAppView::POCSAGAppView(NavigationView& nav) {
                   &sym_ignore,
                   &console});
 
+    if (!settings_.loaded())
+        receiver_model.set_target_frequency(initial_target_frequency);
+
     receiver_model.set_modulation(ReceiverModel::Mode::NarrowbandFMAudio);
     receiver_model.enable();
 
