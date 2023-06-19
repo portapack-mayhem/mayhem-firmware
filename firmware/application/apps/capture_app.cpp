37d0807ab9cccba36e23f679d368d6483fa4610f
Mark Thompson <129641948+NotherNgineer@users.noreply.github.com>
2023-06-18 20:19:14 -0500

    Clang formatting

--- firmware/application/apps/capture_app.cpp
+++ firmware/application/apps/capture_app.cpp
@@ -102,7 +102,8 @@ CaptureAppView::CaptureAppView(NavigationView& nav)
         waterfall.on_show();
     };
 
-    receiver_model.set_sampling_rate(8*500000);  // Set to any valid rate before waterfall.on_hide() code triggered by line below
+    // Set sampling rate to any valid rate before waterfall code is triggered by line below
+    receiver_model.set_sampling_rate(4000000);
     option_bandwidth.set_selected_index(7);  // Preselected default option 500kHz.
 
     receiver_model.set_modulation(ReceiverModel::Mode::Capture);
