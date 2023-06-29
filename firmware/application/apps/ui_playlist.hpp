192fc4dc615f7e717e4250dd70a6842821ded157
zxkmm <zxkmm@hotmail.com>
2023-06-29 23:56:09 +0800

    edit for PR comment - 3

--- firmware/application/apps/ui_playlist.hpp
+++ firmware/application/apps/ui_playlist.hpp
@@ -118,6 +118,11 @@ class PlaylistView : public View {
     Text text_sample_rate{
         {10 * 8, 1 * 16, 7 * 8, 16}};
 
+    /*v making there's 1px line (instead of two) between two progress bars,
+     * by letting 1px overlapped.
+     * So, since they overlapped 1px, they are visually same, and looks better.
+     */
+
     ProgressBar progressbar_track{
         {18 * 8, 1 * 16, 12 * 8, 8 + 1}};
 
