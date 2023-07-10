8507fc825191692de6c43d75b3f9b7ab54da5fee
zxkmm <zxkmm@hotmail.com>
2023-07-10 19:05:50 +0800

    “init_commit_for_fix_changelog”

--- .github/workflows/changelog.py
+++ .github/workflows/changelog.py
@@ -2,18 +2,29 @@ import os
 import re
 import sys
 
-raw_git = os.popen('git log next --since="24 hours" --pretty=format:"- %h - {USERNAME}*+%al-%an*: %s"').read()
+raw_git = os.popen('git log next --since="24 hours" --pretty=format:"- %h - {USERNAME}*_%al_%an*: %s"').read()
+# ^ as github's rule, a real username can contains "-" but not "_" and "*", so use these two to seperate things
 
+# during test at 2023-07-10, %al could return 3 different types of values:
+# 123+abc
+# def@ghi.mgl+abc
+# abc
+# admin
+# ^ abc is real username, note that github will probably change in the future!!!
 
 def compute_username(line):
-    stripped = re.search(r'(?<=\*)(.*?)(?=\*)', line).group(0)
+    stripped = re.search(r'(?<=\*)(.*?)(?=\*)', line).group(0) # get the string between "*" and "*"
+    stripped_org = stripped
+    stripped = re.search(r'(?<=\_)(.*?)(?=\_)', stripped).group(0) # get the string between "_" and "_"
+    # now it's like "123+abc" or "def@ghi+mgl+abc" or "abc" or "admin"
 
-    pattern = re.compile("[$@+&?].*[$@+&?]")
-    if pattern.match(stripped):
-        stripped = re.sub("[$@+&?].*[$@+&?]", "", stripped)
-        stripped = re.match(r'.+?(?=-)', stripped).group(0)
-    else:
-        stripped = re.sub(r'^.*?-', "", stripped)
+    if ("+" in stripped): # like 123+abc or def@ghi.mgl+abc
+        stripped = re.sub(r'^.*?\+', "", stripped)
+    elif (stripped == "admin"):
+        # remove "_admin_" from stripped_org and return "@" + stripped_org
+        stripped = re.sub(r'_admin_', "", stripped_org)
+    else: # like abc or exception
+        pass
     return "@" + stripped
 
 
