2498861003a5303b663b0c10f55100769686df9d
Mark Thompson <129641948+NotherNgineer@users.noreply.github.com>
2023-07-16 17:05:27 -0500

    Increased number of encoder dial sensitivity setting levels (#1275)
    * Encoder dial sensitivity 0-4
    
    * Workaround for worn encoders & more sensitivity levels
    
    * Workaround for worn encoders & more sensitivity levels
    
    * Encoder sensitivity levels 0-4
    
    * Encoder sensitivity levels 0-4
    
    * Fix comment
    
    * Clang
    
    * Clang
    
    * Change default encoder sensitivity in pmem
    
    * Set default encoder sensitivity
    
    * Set default encoder sensitivity
    
    * Revert widget type for encoder sensitivity
    
    * Revert widget type for encoder dial sensitivity
    
    * Revert worn encoder workaround (didn't work)
    
    * 5 levels of encoder dial sensitivity
    
    * 5 levels of encoder dial sensitivity

--- firmware/common/portapack_persistent_memory.cpp
+++ firmware/common/portapack_persistent_memory.cpp
@@ -363,6 +363,7 @@ void defaults() {
 
     set_config_backlight_timer(backlight_config_t{});
     set_config_splash(true);
+    set_encoder_dial_sensitivity(DIAL_SENSITIVITY_NORMAL);
 
     // Default values for recon app.
     set_recon_autosave_freqs(false);
