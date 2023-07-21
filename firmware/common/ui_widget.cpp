2af95456a2e7bf50d91c3cc6953a841965944f3c
Mark Thompson <129641948+NotherNgineer@users.noreply.github.com>
2023-07-21 00:06:37 -0500

    Fix ProgressBar widget overflow (#1288)

--- firmware/common/ui_widget.cpp
+++ firmware/common/ui_widget.cpp
@@ -573,7 +573,7 @@ void ProgressBar::paint(Painter& painter) {
     const auto sr = screen_rect();
     const auto s = style();
 
-    v_scaled = (sr.size().width() * _value) / _max;
+    v_scaled = (sr.size().width() * (uint64_t)_value) / _max;
 
     painter.fill_rectangle({sr.location(), {v_scaled, sr.size().height()}}, style().foreground);
     painter.fill_rectangle({{sr.location().x() + v_scaled, sr.location().y()}, {sr.size().width() - v_scaled, sr.size().height()}}, s.background);
