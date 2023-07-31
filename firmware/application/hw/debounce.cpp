260ffee917fc422c22662ad30708e5ff99d21467
Mark Thompson <129641948+NotherNgineer@users.noreply.github.com>
2023-07-31 18:26:32 -0500

    Clang try again

--- firmware/application/hw/debounce.cpp
+++ firmware/application/hw/debounce.cpp
@@ -108,8 +108,7 @@ bool Debounce::feed(const uint8_t bit) {
                 held_time_ = 0;
             }
         }
-    // Has button been released for at least DEBOUNCE_COUNT ticks?
-    } else if ((history_ & DEBOUNCE_MASK) == 0) {
+    } else if ((history_ & DEBOUNCE_MASK) == 0) {  // Has button been released for at least DEBOUNCE_COUNT ticks?
         //
         // Button is released;
         // Was previous button state 1 (pressed)?
