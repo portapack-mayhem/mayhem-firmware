a4636d7872cbc9c4d0b1cfc83268db293d92a3ac
Brumi-2021 <86470699+Brumi-2021@users.noreply.github.com>
2023-08-18 14:49:22 +0200

    Finalise all low bit rate Capture App with x64 (#1386)
    * Finalise all low bit rate Capture App with x64
    
    * Adding back also 25k BW option

--- firmware/application/ui_record_view.cpp
+++ firmware/application/ui_record_view.cpp
@@ -113,8 +113,7 @@ uint32_t RecordView::set_sampling_rate(uint32_t new_sampling_rate) {
      * They are ok as recorded spectrum indication, but they should not be used by Replay app. (the voice speed will be accelerated)
 
      * We keep original black background in all the correct IQ .C16 files BW's Options. */
-    if ((actual_sampling_rate > 8'000'000) || ((actual_sampling_rate <= 1'600'000) && (oversample_rate > OversampleRate::x8))) {  // yellow REC button means not ok for REC, BW >1Mhz , BW <= 100khz due to NG aliasing.
-        // to be updated or removed in the next PR's, according the achieved extended BW's with good quality bandwith REC limits .
+    if (actual_sampling_rate > 8'000'000) {  // yellow REC button means not ok for REC, BW >1Mhz  (BW from 12k5 till 1Mhz OK for REC and Replay)
         button_record.set_background(ui::Color::yellow());
     } else {
         button_record.set_background(ui::Color::black());
@@ -145,11 +144,11 @@ OversampleRate RecordView::get_oversample_rate(uint32_t sample_rate) {
 
     auto rate = ::get_oversample_rate(sample_rate);
 
-    // Currently proc_capture only supports x8, x16, x32 for decimation.
-    if (rate < OversampleRate::x8)  // clipping while x4 is not implemented yet.
+    // Currently proc_capture only supports /8, /16, /32 for decimation.
+    if (rate < OversampleRate::x8)  // clipping while /4 is not implemented yet  (it will be used >1Mhz onwards when available)
         rate = OversampleRate::x8;
-    else if (rate > OversampleRate::x32)  // clipping while x64 is not implemented yet .
-        rate = OversampleRate::x32;
+    else if (rate > OversampleRate::x64)  // clipping while /128 is not implemented yet , (but it is not necessary for 12k5)
+        rate = OversampleRate::x64;
 
     return rate;
 }
