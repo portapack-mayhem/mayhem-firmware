5135b6404de2055094f0534462ae8ebf780814eb
Mark Thompson <129641948+NotherNgineer@users.noreply.github.com>
2023-10-14 03:28:44 -0500

    Prevent unnecessary CPLD SRAM load on first RX operation (#1494)

--- firmware/application/radio.cpp
+++ firmware/application/radio.cpp
@@ -92,8 +92,8 @@ max2839::MAX2839 second_if_max2839{ssp1_target_max283x};
 static max5864::MAX5864 baseband_codec{ssp1_target_max5864};
 static baseband::CPLD baseband_cpld;
 
-// Set invalid to force the set_direction CPLD workaround to run.
-static rf::Direction direction{-1};
+// load_sram() is called at boot in portapack.cpp, including verify CPLD part, so default direction is Receive
+static rf::Direction direction{rf::Direction::Receive};
 static bool baseband_invert = false;
 static bool mixer_invert = false;
 
