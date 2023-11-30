cca0e18f5a5d3c00e07bad64cc5cd61f3495850e
Totoo <ttotoo@gmail.com>
2023-11-30 12:36:59 +0100

    Weather improvements (#1615)
    * Added Acurite986 protocol
    * Added signal age
    * Added myself to about screen

--- firmware/application/apps/ui_about_simple.cpp
+++ firmware/application/apps/ui_about_simple.cpp
@@ -37,7 +37,7 @@ void AboutView::update() {
                 break;
             case 2:
                 console.writeln("NotherNgineer,zxkmm,u-foka");
-                console.writeln("Netro");
+                console.writeln("Netro,HTotoo");
                 console.writeln("");
                 break;
 
