f60dce7ce432dc39aca3a7726574795da1ea6a56
Brumi-2021 <ea3hqj@gmail.com>
2023-12-08 22:16:54 +0100

    Improved sensitivity/selectivity Weather App

--- firmware/baseband/proc_weather.hpp
+++ firmware/baseband/proc_weather.hpp
@@ -30,6 +30,7 @@
 #include "baseband_thread.hpp"
 #include "rssi_thread.hpp"
 #include "message.hpp"
+#include "dsp_decimate.hpp"
 
 #include "fprotos/weatherprotos.hpp"
 
@@ -39,8 +40,20 @@ class WeatherProcessor : public BasebandProcessor {
     void on_message(const Message* const message) override;
 
    private:
-    static constexpr uint32_t usperTick = 500;  // we nees ms to has to divide by 1000
-    static constexpr size_t baseband_fs = 1'750'000;
+    static constexpr size_t baseband_fs = 4'000'000;  // it works, I think we need to write that master clock in the baseband_threat , even later we decimate it.
+    static constexpr uint32_t usperTick = 500 * 8;    // In current sw , we do not scale it due to clock. We scaled it due to less array buffer sampes due to /8 decimation.
+                                                      // TODO , Pending to investigate , why ticks are not proportional to the SR clock, 500 nseg (2Mhz) , 250 nseg (4Mhz) ??? ;previous comment :  "we nees ms to has to divide by 1000"
+
+    /* Array Buffer aux. used in decim0 and decim1 IQ c16 signed  data ; (decim0 defines the max length of the array) */
+    std::array<complex16_t, 512> dst{};  // decim0 /4 ,  2048/4 = 512 complex I,Q
+    const buffer_c16_t dst_buffer{
+        dst.data(),
+        dst.size()};
+
+    /* Decimates */
+    dsp::decimate::FIRC8xR16x24FS4Decim4 decim_0{};
+    dsp::decimate::FIRC16xR16x16Decim2 decim_1{};
+
     uint32_t currentDuration = 0;
     uint32_t threshold = 0x0630;  // will overwrite after the first iteration
     bool currentHiLow = false;
