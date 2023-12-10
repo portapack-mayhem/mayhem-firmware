5e9d92d65ce0b1df70b81df3ba1836af9e48b12a
Totoo <ttotoo@gmail.com>
2023-12-10 23:40:11 +0100

    Set sr to 4m (#1636)
    * Set sr to 4m
    
    * Rename variable

--- firmware/application/apps/ui_weatherstation.cpp
+++ firmware/application/apps/ui_weatherstation.cpp
@@ -101,6 +101,7 @@ WeatherView::WeatherView(NavigationView& nav)
         nav_.push<WeatherRecentEntryDetailView>(entry);
     };
     baseband::set_weather();
+    receiver_model.set_sampling_rate(4'000'000);  // needed too
     receiver_model.enable();
     signal_token_tick_second = rtc_time::signal_tick_second += [this]() {
         on_tick_second();
