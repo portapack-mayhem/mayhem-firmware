e4584d7c633451c74748c284489f663262bf49ca
Mark Thompson <129641948+NotherNgineer@users.noreply.github.com>
2024-02-10 09:28:58 -0600

    Updated copyright comment

--- firmware/application/debug.hpp
+++ firmware/application/debug.hpp
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2015 Jared Boone, ShareBrained Technology, Inc.
  * Copyright (C) 2023 Bernd Herzog
+ * Copyright (C) 2023 Mark Thompson
  *
  * This file is part of PortaPack.
  *
