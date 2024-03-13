61dc8a0225d68a71f81816958dfa29c417b4616c
Mark Thompson <129641948+NotherNgineer@users.noreply.github.com>
2024-03-13 10:08:52 -0500

    Disabled unused baseband "no-op" process (#1986)

--- firmware/application/external/tetris/main.cpp
+++ firmware/application/external/tetris/main.cpp
@@ -76,7 +76,7 @@ __attribute__((section(".external_app.app_tetris.application_information"), used
     /*.icon_color = */ ui::Color::orange().v,
     /*.menu_location = */ app_location_t::UTILITIES,
 
-    /*.m4_app_tag = portapack::spi_flash::image_tag_noop */ {'\0', '\0', '\0', '\0'},  // optional
-    /*.m4_app_offset = */ 0x00000000,                                                  // will be filled at compile time
+    /*.m4_app_tag = portapack::spi_flash::image_tag_none */ {0, 0, 0, 0},
+    /*.m4_app_offset = */ 0x00000000,  // will be filled at compile time
 };
 }
