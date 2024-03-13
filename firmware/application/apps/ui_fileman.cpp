3a3c79a8ab0d69e2fc7bba7ebc98e278733a7f21
zxkmm <zxkmm@hotmail.com>
2024-03-14 01:24:06 +0800

    decided to let user can pop dir even in file picking view

--- firmware/application/apps/ui_fileman.cpp
+++ firmware/application/apps/ui_fileman.cpp
@@ -276,8 +276,17 @@ void FileManBaseView::push_fake_dir(const fs::path& path) {
 }
 
 void FileManBaseView::pop_dir() {
-    if (saved_index_stack.empty())
+    fs::path first_level = current_path.extract_first_level();
+    fs::path root_dir = u"/";
+    fs::path null_dir = u"";
+
+    if (saved_index_stack.empty() && first_level == null_dir) {
+        current_path = root_dir;
+        saved_index_stack.clear();
+
+    } else if (saved_index_stack.empty() && first_level == root_dir) {
         return;
+    }
 
     current_path = current_path.parent_path();
 
