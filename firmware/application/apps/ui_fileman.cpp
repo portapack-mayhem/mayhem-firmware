portapack-mayhem/mayhem-firmware

commit 3a3c79a8ab0d69e2fc7bba7ebc98e278733a7f21

decided to let user can pop dir even in file picking view

firmware/application/apps/ui_fileman.cpp
@@ -276,8 +276,17 @@ void FileManBaseView::push_fake_dir(const fs::path& path) {
 }
 
 void FileManBaseView::pop_dir() {
-    if (saved_index_stack.empty())
+    fs::path first_level = current_path.extract_first_level();
+    fs::path root_dir = u"/";
+    fs::path null_dir = u"";
+
+    if (saved_index_stack.empty() && first_level == null_dir) {
+        current_path = root_dir;
+        saved_index_stack.clear();
+
+    } else if (saved_index_stack.empty() && first_level == root_dir) {
         return;
+    }
 
     current_path = current_path.parent_path();
 

commit 5181923fc6fcad9220a7449dfe3934794adc892b

fixed bug caused by 3a3c79a8ab0d69e2fc7bba7ebc98e278733a7f21

firmware/application/apps/ui_fileman.cpp
@@ -276,15 +276,17 @@ void FileManBaseView::push_fake_dir(const fs::path& path) {
 }
 
 void FileManBaseView::pop_dir() {
-    if (saved_index_stack.empty()) {
+    if (saved_index_stack.empty() && current_path == u"/") {
         return;
+    } else if (saved_index_stack.empty()) {
+        current_path = u"";
+        reload_current();
+    } else {
+        current_path = current_path.parent_path();
+        reload_current();
+        menu_view.set_highlighted(saved_index_stack.back());
+        saved_index_stack.pop_back();
     }
-
-    current_path = current_path.parent_path();
-
-    reload_current();
-    menu_view.set_highlighted(saved_index_stack.back());
-    saved_index_stack.pop_back();
 }
 
 void FileManBaseView::refresh_list() {