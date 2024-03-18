8383363e741cf9ddeb7b3187686811285430a49c
Mark Thompson <129641948+NotherNgineer@users.noreply.github.com>
2024-03-18 15:45:02 -0500

    Preserve radio mode when updating iq_tx_phase_cal (#2008)

--- firmware/application/hw/max2839.cpp
+++ firmware/application/hw/max2839.cpp
@@ -143,6 +143,8 @@ void MAX2839::init() {
 }
 
 void MAX2839::set_tx_LO_iq_phase_calibration(const size_t v) {
+    Mode saved_mode = get_mode();
+
     /* IQ phase deg CAL adj (+4 ...-4)  This IC  in 64 steps (6 bits), 000000 = +4deg (Q lags I by 94degs, default), 011111 = +0deg, 111111 = -4deg (Q lags I by 86degs) */
 
     // TX calibration , 2 x Logic pins , ENABLE, RXENABLE = 1,0, (2dec), and  Reg address 16, D1 (CAL mode 1):DO (CHIP ENABLE 1)
@@ -164,7 +166,7 @@ void MAX2839::set_tx_LO_iq_phase_calibration(const size_t v) {
     _map.r.spi_en.EN_SPI = 1;   // Register Settings reg address 16,  DO (1 = Chip select enable )
     flush_one(Register::SPI_EN);
 
-    set_mode(Mode::Standby);  // Back 3  logic pins CALIBRATION mode -> Standby.
+    set_mode(saved_mode);  // restore original mode
 }
 
 enum class Mask {
@@ -196,9 +198,15 @@ Mask mode_mask(const Mode mode) {
 }
 
 void MAX2839::set_mode(const Mode mode) {
+    _mode = mode;
+
     Mask mask = mode_mask(mode);
     gpio_max283x_enable.write(toUType(mask) & toUType(Mask::Enable));
     gpio_max2839_rxtx.write(toUType(mask) & toUType(Mask::RxTx));
+}
+
+Mode MAX2839::get_mode() {
+    return _mode;
 }
 
 void MAX2839::flush() {
