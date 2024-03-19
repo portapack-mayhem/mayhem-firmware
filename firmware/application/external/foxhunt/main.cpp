c79ef4b061453a988c62332bf3dba30d3ad748ef
HTotoo <ttotoo@gmail.com>
2024-03-19 11:48:42 +0100

    cr info

--- firmware/application/external/foxhunt/main.cpp
+++ firmware/application/external/foxhunt/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Bernd Herzog
+ * Copyright (C) 2024 HTotoo
  *
  * This file is part of PortaPack.
  *
--- firmware/application/external/foxhunt/ui_foxhunt_rx.cpp
+++ firmware/application/external/foxhunt/ui_foxhunt_rx.cpp
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2014 Jared Boone, ShareBrained Technology, Inc.
- * Copyright (C) 2017 Furrtek
+ * Copyright (C) 2024 HTotoo
  *
  * This file is part of PortaPack.
  *
