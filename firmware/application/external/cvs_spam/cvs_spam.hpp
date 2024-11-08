5f5c3115ca3a22fe8f513cb153526a2ffa4bf3f8
RocketGod-git <jeremy@umcontractors.com>
2024-11-08 13:14:11 -0800

    indentation

--- firmware/application/external/cvs_spam/cvs_spam.hpp
+++ firmware/application/external/cvs_spam/cvs_spam.hpp
@@ -1,6 +1,6 @@
 // CVS Spam app by RocketGod (@rocketgod-git) https://betaskynet.com
 // Original .cu8 files by @jimilinuxguy https://github.com/jimilinuxguy/customer-assistance-buttons-sdr
-// If you can read this, you're a nerd. :P  
+// If you can read this, you're a nerd. :P
 // Come join us at https://discord.gg/thepiratesreborn
 
 #pragma once
@@ -18,99 +18,98 @@ using namespace portapack;
 namespace ui::external_app::cvs_spam {
 
 class CVSSpamView : public View {
-public:
-  explicit CVSSpamView(NavigationView& nav);
-  ~CVSSpamView();
+   public:
+    explicit CVSSpamView(NavigationView& nav);
+    ~CVSSpamView();
 
-  void focus() override;
-  std::string title() const override { return "CVS Spam"; };
+    void focus() override;
+    std::string title() const override { return "CVS Spam"; };
 
-private:
-  static constexpr size_t read_size = 0x4000;
-  static constexpr size_t buffer_count = 3;
+   private:
+    static constexpr size_t read_size = 0x4000;
+    static constexpr size_t buffer_count = 3;
 
-  NavigationView& nav_;
-  std::unique_ptr<ReplayThread> replay_thread{};
-  bool ready_signal{false};
-  bool thread_sync_complete{false};
-  std::filesystem::path current_file; 
+    NavigationView& nav_;
+    std::unique_ptr<ReplayThread> replay_thread{};
+    bool ready_signal{false};
+    bool thread_sync_complete{false};
+    std::filesystem::path current_file;
 
-  bool is_active() const;
-  void stop_tx();
-  void file_error(const std::filesystem::path& path, const std::string& error_details); 
-  void refresh_list();
-  void start_tx(const uint32_t id);
-  void start_chaos_tx();
-  void on_tx_progress(const uint32_t progress);
+    bool is_active() const;
+    void stop_tx();
+    void file_error(const std::filesystem::path& path, const std::string& error_details);
+    void refresh_list();
+    void start_tx(const uint32_t id);
+    void start_chaos_tx();
+    void on_tx_progress(const uint32_t progress);
 
-  uint32_t page = 1;
-  uint32_t current_page = 1;
-  std::vector<std::filesystem::path> file_list{};
+    uint32_t page = 1;
+    uint32_t current_page = 1;
+    std::vector<std::filesystem::path> file_list{};
 
-  MenuView menu_view{
-      {0, 0, 240, 180},
-      true};
+    MenuView menu_view{
+        {0, 0, 240, 180},
+        true};
 
-  Text text_empty{
-      {7 * 8, 12 * 8, 16 * 8, 16},
-      "Empty directory!"
-  };
+    Text text_empty{
+        {7 * 8, 12 * 8, 16 * 8, 16},
+        "Empty directory!"};
 
-  Button button_prev_page{
-      {0, 180, 50, 32},
-      "<"};
+    Button button_prev_page{
+        {0, 180, 50, 32},
+        "<"};
 
-  Button button_next_page{
-      {190, 180, 50, 32},
-      ">"};
+    Button button_next_page{
+        {190, 180, 50, 32},
+        ">"};
 
-  Text page_info{
-      {95, 188, 50, 16}};
+    Text page_info{
+        {95, 188, 50, 16}};
 
-  Button button_send{
-      {0, 212, 75, 36},
-      "CALL"};
+    Button button_send{
+        {0, 212, 75, 36},
+        "CALL"};
 
-  Button button_chaos{
-      {82, 212, 75, 36},
-      "CHAOS"};
+    Button button_chaos{
+        {82, 212, 75, 36},
+        "CHAOS"};
 
-  Button button_stop{
-      {165, 212, 75, 36},
-      LanguageHelper::currentMessages[LANG_STOP]};
+    Button button_stop{
+        {165, 212, 75, 36},
+        LanguageHelper::currentMessages[LANG_STOP]};
 
-  ProgressBar progressbar{
-      {0, 256, 240, 44}};
+    ProgressBar progressbar{
+        {0, 256, 240, 44}};
 
-  MessageHandlerRegistration message_handler_fifo_signal{
-      Message::ID::RequestSignal,
-      [this](const Message* const p) {
-          const auto message = static_cast<const RequestSignalMessage*>(p);
-          if (message->signal == RequestSignalMessage::Signal::FillRequest) {
-              ready_signal = true;
-          }
-      }};
+    MessageHandlerRegistration message_handler_fifo_signal{
+        Message::ID::RequestSignal,
+        [this](const Message* const p) {
+            const auto message = static_cast<const RequestSignalMessage*>(p);
+            if (message->signal == RequestSignalMessage::Signal::FillRequest) {
+                ready_signal = true;
+            }
+        }};
 
-  MessageHandlerRegistration message_handler_tx_progress{
-      Message::ID::TXProgress,
-      [this](const Message* const p) {
-          const auto message = *reinterpret_cast<const TXProgressMessage*>(p);
-          this->on_tx_progress(message.progress);
-      }};
+    MessageHandlerRegistration message_handler_tx_progress{
+        Message::ID::TXProgress,
+        [this](const Message* const p) {
+            const auto message = *reinterpret_cast<const TXProgressMessage*>(p);
+            this->on_tx_progress(message.progress);
+        }};
 
-   MessageHandlerRegistration message_handler_replay_thread_done{
-       Message::ID::ReplayThreadDone,
-       [this](const Message* const p) {
-           const auto message = *reinterpret_cast<const ReplayThreadDoneMessage*>(p);
-           if (message.return_code == ReplayThread::END_OF_FILE) {
-               if (is_active()) {
-                   thread_sync_complete = true;
-                   stop_tx();
-               }
-           } else if (message.return_code == ReplayThread::READ_ERROR) {
-               file_error(file_list[menu_view.highlighted_index()], "Read error during playback");
-           }
-       }};
+    MessageHandlerRegistration message_handler_replay_thread_done{
+        Message::ID::ReplayThreadDone,
+        [this](const Message* const p) {
+            const auto message = *reinterpret_cast<const ReplayThreadDoneMessage*>(p);
+            if (message.return_code == ReplayThread::END_OF_FILE) {
+                if (is_active()) {
+                    thread_sync_complete = true;
+                    stop_tx();
+                }
+            } else if (message.return_code == ReplayThread::READ_ERROR) {
+                file_error(file_list[menu_view.highlighted_index()], "Read error during playback");
+            }
+        }};
 };
 
-}
+}  // namespace ui::external_app::cvs_spam
--- firmware/application/external/cvs_spam/main.cpp
+++ firmware/application/external/cvs_spam/main.cpp
@@ -1,6 +1,6 @@
 // CVS Spam app by RocketGod (@rocketgod-git) https://betaskynet.com
 // Original .cu8 files by @jimilinuxguy https://github.com/jimilinuxguy/customer-assistance-buttons-sdr
-// If you can read this, you're a nerd. :P  
+// If you can read this, you're a nerd. :P
 // Come join us at https://discord.gg/thepiratesreborn
 
 #include "ui.hpp"
@@ -10,28 +10,24 @@
 
 namespace ui::external_app::cvs_spam {
 void initialize_app(NavigationView& nav) {
-   nav.push<CVSSpamView>();
+    nav.push<CVSSpamView>();
 }
-}
+}  // namespace ui::external_app::cvs_spam
 
 extern "C" {
 
 __attribute__((section(".external_app.app_cvs_spam.application_information"), used)) application_information_t _application_information_cvs_spam = {
-   (uint8_t*)0x00000000,
-   ui::external_app::cvs_spam::initialize_app,
-   CURRENT_HEADER_VERSION,
-   VERSION_MD5,
-   "CVS Spam",
-   {
-       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-       0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-       0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0x81
-   },
-   ui::Color::red().v,
-   app_location_t::TX,
-   {'P', 'R', 'E', 'P'},
-   0x00000000
-};
-
+    (uint8_t*)0x00000000,
+    ui::external_app::cvs_spam::initialize_app,
+    CURRENT_HEADER_VERSION,
+    VERSION_MD5,
+    "CVS Spam",
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+     0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+     0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0x81},
+    ui::Color::red().v,
+    app_location_t::TX,
+    {'P', 'R', 'E', 'P'},
+    0x00000000};
 }
