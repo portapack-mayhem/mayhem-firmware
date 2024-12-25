9f4c4141c9c2c8f9f295a58d8724be46e252af88
zxkmm <zxkmm@hotmail.com>
2024-12-25 11:27:56 +0800

    clean up - remove unneeded import again

--- firmware/application/external/app_manager/ui_app_manager.hpp
+++ firmware/application/external/app_manager/ui_app_manager.hpp
@@ -1,10 +1,28 @@
+/*
+ * copyleft spammingdramaqueen
+ *
+ * This file is part of PortaPack.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
 #ifndef __UI_APP_MANAGER_H__
 #define __UI_APP_MANAGER_H__
 
 #include "ui_navigation.hpp"
-#include "ui_receiver.hpp"
-#include "audio.hpp"
-#include "ch.h"
 
 namespace ui::external_app::app_manager {
 
