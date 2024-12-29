80b9793bac3a9bc3976cf09a4fc68b8f41f06609
Mark Thompson <129641948+NotherNgineer@users.noreply.github.com>
2024-12-28 23:02:34 -0600

    Remember previous capture settings

--- firmware/application/apps/capture_app.cpp
+++ firmware/application/apps/capture_app.cpp
@@ -55,12 +55,15 @@ CaptureAppView::CaptureAppView(NavigationView& nav)
         this->field_frequency.set_step(v);
     };
 
-    option_format.set_selected_index(0);  // Default to C16
+    option_format.set_selected_index(previous_format);
     option_format.on_change = [this](size_t, uint32_t file_type) {
+        previous_format = file_type;
         record_view.set_file_type((RecordView::FileType)file_type);
     };
 
+    check_trim.set_value(previous_trim);
     check_trim.on_select = [this](Checkbox&, bool v) {
+        previous_trim = v;
         record_view.set_auto_trim(v);
     };
 
@@ -100,7 +103,7 @@ CaptureAppView::CaptureAppView(NavigationView& nav)
     };
 
     receiver_model.enable();
-    option_bandwidth.set_by_value(500000);
+    option_bandwidth.set_by_value(previous_bandwidth);
 
     record_view.on_error = [&nav](std::string message) {
         nav.display_modal("Error", message);
