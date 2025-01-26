520ad97f7881974521a4499f962e908e10a1294a
Davide Rovelli <103165301+daviderud@users.noreply.github.com>
2025-01-26 21:41:11 +0100

    Added different modulations in signal generator (#2492)
    * Added DSB, AM 100% mod index and AM 50% mod index. Changed UI.

--- firmware/application/apps/ui_siggen.hpp
+++ firmware/application/apps/ui_siggen.hpp
@@ -58,67 +58,72 @@ class SigGenView : public View {
     app_settings::SettingsManager settings_{
         "tx_siggen", app_settings::Mode::TX};
 
-    const std::string shape_strings[9] = {
-        "CW  (No mod.) ",
-        "Sine mod. FM",
-        "Triangle mod.FM",  // max 15 character text space.
-        "Saw up mod. FM",
-        "Saw down mod.FM",
-        "Square mod. FM",
-        "Pseudo Noise FM",  // using 16 bits LFSR register, 16 order polynomial feedback.
-        "BPSK 0,1,0,1...",
-        "QPSK 00-01-10.."};
+    const std::string shape_strings[6] = {// max 15 character text space.
+                                          "Sine",
+                                          "Triangle",
+                                          "Saw up",
+                                          "Saw down",
+                                          "Square",
+                                          "Pseudo Noise"};
 
     bool auto_update{false};
 
     Labels labels{
-        {{3 * 8, 4 + 10}, "Shape:", Theme::getInstance()->fg_light->foreground},
-        {{6 * 8, 7 * 8}, "Tone:      Hz", Theme::getInstance()->fg_light->foreground},
-        {{22 * 8, 15 * 8 + 4}, "s.", Theme::getInstance()->fg_light->foreground},
-        {{8 * 8, 20 * 8}, "Modulation: FM", Theme::getInstance()->fg_light->foreground}};
+        {{3 * 8, 2 * 8}, "Modulation:", Theme::getInstance()->fg_light->foreground},
+        {{3 * 8, 3 * 8 + 8 + 10}, "Shape:", Theme::getInstance()->fg_light->foreground},
+        {{6 * 8, 2 * 8 + 7 * 8}, "Tone:      Hz", Theme::getInstance()->fg_light->foreground},
+        {{22 * 8, 2 * 8 + 15 * 8 + 4}, "s.", Theme::getInstance()->fg_light->foreground}};
 
     ImageOptionsField options_shape{
-        {10 * 8, 4, 32, 32},
+        {10 * 8, 3 * 8 + 8, 32, 32},
         Theme::getInstance()->bg_darkest->foreground,
         Theme::getInstance()->bg_darkest->background,
-        {{&bitmap_sig_cw, 0},
-         {&bitmap_sig_sine, 1},
-         {&bitmap_sig_tri, 2},
-         {&bitmap_sig_saw_up, 3},
-         {&bitmap_sig_saw_down, 4},
-         {&bitmap_sig_square, 5},
-         {&bitmap_sig_noise, 6},
-         {&bitmap_sig_noise, 7},    // Pending to add a correct BPSK icon.
-         {&bitmap_sig_noise, 8}}};  // Pending to add a correct QPSK icon.
+        {{&bitmap_sig_sine, 0},
+         {&bitmap_sig_tri, 1},
+         {&bitmap_sig_saw_up, 2},
+         {&bitmap_sig_saw_down, 3},
+         {&bitmap_sig_square, 4},
+         {&bitmap_sig_noise, 5}}};
 
     Text text_shape{
-        {15 * 8, 4 + 10, 15 * 8, 16},
+        {15 * 8, 3 * 8 + 8 + 10, 15 * 8, 16},
         ""};
 
     SymField symfield_tone{
-        {12 * 8, 7 * 8},
+        {12 * 8, 2 * 8 + 7 * 8},
         5};
 
     Button button_update{
-        {5 * 8, 10 * 8, 8 * 8, 3 * 8},
+        {5 * 8, 2 * 8 + 10 * 8, 8 * 8, 3 * 8},
         "Update"};
 
     Checkbox checkbox_auto{
-        {15 * 8, 10 * 8},
+        {15 * 8, 2 * 8 + 10 * 8},
         4,
         "Auto"};
 
     Checkbox checkbox_stop{
-        {5 * 8, 15 * 8},
+        {5 * 8, 2 * 8 + 15 * 8},
         10,
         "Stop after"};
 
     NumberField field_stop{
-        {20 * 8, 15 * 8 + 4},
+        {20 * 8, 2 * 8 + 15 * 8 + 4},
         2,
         {1, 99},
         1,
         ' '};
+
+    OptionsField options_mod{
+        {15 * 8, 2 * 8},
+        12,
+        {{"CW (No mod.)", 0},
+         {"FM", 1},
+         {"BPSK", 2},
+         {"QPSK", 3},
+         {"DSB", 4},
+         {"AM 100% dep.", 5},
+         {"AM 50% depth", 6}}};
 
     TransmitterView tx_view{
         16 * 16,
