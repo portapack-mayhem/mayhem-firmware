5061ec816709c7647780535e8b81d27a8fac18dd
gullradriel <gullradriel@no-mail.com>
2025-04-04 15:36:45 +0200

    persistent settings and no more global living variables

--- firmware/application/apps/analog_audio_app.hpp
+++ firmware/application/apps/analog_audio_app.hpp
@@ -199,6 +199,12 @@ class AnalogAudioView : public View {
     uint8_t get_zoom_factor(uint8_t mode);
     void set_zoom_factor(uint8_t mode, uint8_t zoom);
 
+    uint8_t get_previous_AM_mode_option();
+    void set_previous_AM_mode_option(uint8_t mode);
+
+    uint8_t get_previous_zoom_option();
+    void set_previous_zoom_option(uint8_t zoom);
+
    private:
     static constexpr ui::Dim header_height = 3 * 16;
 
@@ -207,6 +213,9 @@ class AnalogAudioView : public View {
     uint8_t iq_phase_calibration_value{15};  // initial default RX IQ phase calibration value , used for both max2837 & max2839
     uint8_t zoom_factor_am{0};               // initial zoom factor in AM mode
     uint8_t zoom_factor_amfm{0};             // initial zoom factor in AMFM mode
+    uint8_t previous_AM_mode_option{0};      // GUI 5 AM modes :  (0..4 ) (DSB9K, DSB6K, USB,LSB, CW). Used to select proper FIR filter (0..11) AM mode  + offset 0 (zoom+1) or +6 (if zoom+2)
+    uint8_t previous_zoom{0};                // GUI ZOOM+1, ZOOM+2 , equivalent to two values offset 0 (zoom+1) or +6 (if zoom+2)
+                                             //
     app_settings::SettingsManager settings_{
         "rx_audio",
         app_settings::Mode::RX,
@@ -214,6 +223,8 @@ class AnalogAudioView : public View {
             {"iq_phase_calibration"sv, &iq_phase_calibration_value},  // we are saving and restoring that CAL from Settings.
             {"zoom_factor_am"sv, &zoom_factor_am},                    // we are saving and restoring AM ZOOM factor from Settings.
             {"zoom_factor_amfm"sv, &zoom_factor_amfm},                // we are saving and restoring AMFM ZOOM factor from Settings.
+            {"previous_AM_mode_option"sv, &previous_AM_mode_option},  // we are saving and restoring AMFM ZOOM factor from Settings.
+            {"previous_zoom"sv, &previous_zoom},                      // we are saving and restoring AMFM ZOOM factor from Settings.
         }};
 
     const Rect options_view_rect{0 * 8, 1 * 16, 30 * 8, 1 * 16};
