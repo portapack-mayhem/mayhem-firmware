e177b2d288cd03336b38c099ec0f446b192b5014
zxkmm <zxkmm@hotmail.com>
2025-04-16 22:16:20 +0800

    format

--- firmware/application/ui_navigation.cpp
+++ firmware/application/ui_navigation.cpp
@@ -414,7 +414,7 @@ void SystemStatusView::refresh() {
     button_converter.set_foreground(pmem::config_converter() ? Theme::getInstance()->fg_red->foreground : Theme::getInstance()->fg_light->foreground);
 
     // Fake Brightness
-    button_fake_brightness.set_foreground(pmem::apply_fake_brightness()  ? *Theme::getInstance()->status_active : Theme::getInstance()->fg_light->foreground);
+    button_fake_brightness.set_foreground(pmem::apply_fake_brightness() ? *Theme::getInstance()->status_active : Theme::getInstance()->fg_light->foreground);
 
     set_dirty();
 }
