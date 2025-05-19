66e9745a1c0b601f6621d982f83859ecad05b7b4
zxkmm <zxkmm@hotmail.com>
2025-05-19 11:18:18 +0800

    display now, TODO: mirror, zoom

--- firmware/common/lcd_ili9341.cpp
+++ firmware/common/lcd_ili9341.cpp
@@ -102,74 +102,21 @@ void lcd_init() {
     // LCDs are configured for IM[2:0] = 001
     // 8080-I system, 16-bit parallel bus
 
-    //
-    // 0x3a: DBI[2:0] = 101
-    // MDT[1:0] = XX (if not in 18-bit mode, right?)
+    // The reset sequence is already handled by lcd_reset() before this function is called
 
-    // Power control B
-    // 0
-    // PCEQ=1, DRV_ena=0, Power control=3
-    io.lcd_data_write_command_and_data(0xCF, {0x00, 0xD9, 0x30});
+    // Start Initial Sequence based on provided example code
+    io.lcd_data_write_command_and_data(0xE0, {0x00, 0x09, 0x0C, 0x03, 0x10, 0x06, 0x34, 0x68,
+                                              0x49, 0x02, 0x0A, 0x07, 0x2C, 0x31, 0x0F});
 
-    // Power on sequence control
-    io.lcd_data_write_command_and_data(0xED, {0x64, 0x03, 0x12, 0x81});
+    io.lcd_data_write_command_and_data(0xE1, {0x00, 0x12, 0x15, 0x02, 0x10, 0x06, 0x35, 0x35,
+                                              0x4A, 0x05, 0x10, 0x0C, 0x2F, 0x33, 0x0F});
 
-    // Driver timing control A
-    io.lcd_data_write_command_and_data(0xE8, {0x85, 0x10, 0x78});
+    io.lcd_data_write_command_and_data(0xC0, {0x0F, 0x0F});  // Vreg1out=4.5 Vreg2out=-4.5
 
-    // Power control A
-    io.lcd_data_write_command_and_data(0xCB, {0x39, 0x2C, 0x00, 0x34, 0x02});
+    io.lcd_data_write_command_and_data(0xC1, {0x44});  // VGH = 5*VCI VGL = -4*VCI
 
-    // Pump ratio control
-    io.lcd_data_write_command_and_data(0xF7, {0x20});
+    io.lcd_data_write_command_and_data(0xC5, {0x00, 0x66, 0x80});  // VCOM
 
-    // Driver timing control B
-    io.lcd_data_write_command_and_data(0xEA, {0x00, 0x00});
-
-    io.lcd_data_write_command_and_data(0xB1, {0x00, 0x1B});
-
-    // Blanking Porch Control
-    // VFP = 0b0000010 = 2 (number of HSYNC of vertical front porch)
-    // VBP = 0b0000010 = 2 (number of HSYNC of vertical back porch)
-    // HFP = 0b0001010 = 10 (number of DOTCLOCK of horizontal front porch)
-    // HBP = 0b0010100 = 20 (number of DOTCLOCK of horizontal back porch)
-    io.lcd_data_write_command_and_data(0xB5, {0x02, 0x02, 0x0a, 0x14});
-
-    // Display Function Control
-    // PT[1:0] = 0b10
-    // PTG[1:0] = 0b10
-    // ISC[3:0] = 0b0010 (scan cycle interval of gate driver: 5 frames)
-    // SM = 0 (gate driver pin arrangement in combination with GS)
-    // SS = 1 (source output scan direction S720 -> S1)
-    // GS = 0 (gate output scan direction G1 -> G320)
-    // REV = 1 (normally white)
-    // NL = 0b100111 (default)
-    // PCDIV = 0b000000 (default?)
-
-    /*as per the datasheet chapter 8.3.7, addr B6h,
-    data "REV" bit, liquid crystal type:*/
-    if (portapack::persistent_memory::config_lcd_normally_black())
-        io.lcd_data_write_command_and_data(0xB6, {0x0A, 0x22, 0x27, 0x00});  // IPS : normally black : 0
-    else
-        io.lcd_data_write_command_and_data(0xB6, {0x0A, 0xA2, 0x27, 0x00});  // TFT : normally white : 1
-
-    // Power Control 1
-    // VRH[5:0]
-    io.lcd_data_write_command_and_data(0xC0, {0x1B});
-
-    // Power Control 2
-    // SAP[2:0];BT[3:0]
-    io.lcd_data_write_command_and_data(0xC1, {0x12});
-
-    // VCOM Control 1
-    io.lcd_data_write_command_and_data(0xC5, {0x32, 0x3C});
-
-    // VCOM Control 2
-    io.lcd_data_write_command_and_data(0xC7, {0x9B});
-
-    // Memory Access Control
-    // Invert X and Y memory access order, so upper-left of
-    // screen is (0,0) when writing to display.
     io.lcd_data_write_command_and_data(0x36, {
                                                  (1 << 7) |  // MY=1
                                                  (1 << 6) |  // MX=1
@@ -178,35 +125,27 @@ void lcd_init() {
                                                  (1 << 3)    // BGR=1: For Kingtech LCD, BGR filter.
                                              });
 
-    // COLMOD: Pixel Format Set
-    // DPI=101 (16 bits/pixel), DBI=101 (16 bits/pixel)
+    // io.lcd_data_write_command_and_data(0x36, {0x48});
+
     io.lcd_data_write_command_and_data(0x3A, {0x55});
 
-    // io.lcd_data_write_command_and_data(0xF6, { 0x01, 0x30 });
-    //  WEMODE=1 (reset column and page number on overflow)
-    //  MDT[1:0]
-    //  EPF[1:0]=00 (use channel MSB for LSB)
-    //  RIM=0 (If COLMOD[6:4]=101 (65k color), 16-bit RGB interface (1 transfer/pixel))
-    //  RM=0 (system interface/VSYNC interface)
-    //  DM[1:0]=00 (internal clock operation)
-    //  ENDIAN=0 (doesn't matter with 16-bit interface)
-    io.lcd_data_write_command_and_data(0xF6, {0x01, 0x30, 0x00});
+    io.lcd_data_write_command_and_data(0xB1, {0xA0, 0x11});  // Frame rate
 
-    // 3Gamma Function Disable
-    io.lcd_data_write_command_and_data(0xF2, {0x00});
+    io.lcd_data_write_command_and_data(0xB4, {0x02});  // 2 dot inversion
 
-    // Gamma curve selected
-    io.lcd_data_write_command_and_data(0x26, {0x01});
+    io.lcd_data_write_command_and_data(0xEE, {0x00, 0x04});
 
-    // Set Gamma
-    io.lcd_data_write_command_and_data(0xE0, {0x0F, 0x1D, 0x19, 0x0E, 0x10, 0x07, 0x4C, 0x63,
-                                              0x3F, 0x03, 0x0D, 0x00, 0x26, 0x24, 0x04});
+    io.lcd_data_write_command_and_data(0xE9, {0x00});
 
-    // Set Gamma
-    io.lcd_data_write_command_and_data(0xE1, {0x00, 0x1C, 0x1F, 0x02, 0x0F, 0x03, 0x35, 0x25,
-                                              0x47, 0x04, 0x0C, 0x0B, 0x29, 0x2F, 0x05});
+    io.lcd_data_write_command_and_data(0xF7, {0xA9, 0x51, 0x2C, 0x82});
 
-    lcd_wake();
+    io.lcd_data_write_command_and_data(0x21, {});  // Display Inversion On
+
+    io.lcd_data_write_command_and_data(0x11, {});  // Sleep Out
+    chThdSleepMilliseconds(120);                   // Delay 120ms
+
+    io.lcd_data_write_command_and_data(0x29, {});  // Display On
+    chThdSleepMilliseconds(50);                    // Delay 50ms
 
     // Turn on Tearing Effect Line (TE) output signal.
     io.lcd_data_write_command_and_data(0x35, {0b00000000});
