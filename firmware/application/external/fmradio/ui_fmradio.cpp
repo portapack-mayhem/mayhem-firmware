e9e280954cbb9c2ce8f1bb871ca53a0b4f0a681b
HTotoo <ttotoo@gmail.com>
2025-06-06 11:37:28 +0200

    widgetize

--- firmware/application/external/fmradio/ui_fmradio.cpp
+++ firmware/application/external/fmradio/ui_fmradio.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 HT Otto
+ * Copyright (C) 2024 HTotoo
  * Copyright (C) 2025 RocketGod - Added modes from my Flipper Zero RF Jammer App - https://betaskynet.com
  *
  * This file is part of PortaPack.
