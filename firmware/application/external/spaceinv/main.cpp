54301d4d2ba28603b0e5f9f001923b2f380e4521
RocketGod-git <rocketgod666@gmail.com>
2025-06-20 13:02:49 -0700

    Format code, sigh.

--- firmware/application/external/spaceinv/main.cpp
+++ firmware/application/external/spaceinv/main.cpp
@@ -6,65 +6,64 @@
  * ------------------------------------------------------------
  */
 
- #include "ui.hpp"
- #include "ui_spaceinv.hpp"
- #include "ui_navigation.hpp"
- #include "external_app.hpp"
- 
- namespace ui::external_app::spaceinv {
- void initialize_app(ui::NavigationView& nav) {
-     nav.push<SpaceInvadersView>();
- }
- }  // namespace ui::external_app::spaceinv
- 
- extern "C" {
- 
- __attribute__((section(".external_app.app_spaceinv.application_information"), used)) application_information_t _application_information_spaceinv = {
-     (uint8_t*)0x00000000,
-     ui::external_app::spaceinv::initialize_app,
-     CURRENT_HEADER_VERSION,
-     VERSION_MD5,
- 
-     "Space Invaders",
-     {
-         0x18,
-         0x3C,
-         0x7E,
-         0xDB,
-         0xFF,
-         0xFF,
-         0x24,
-         0x66,
-         0x42,
-         0x00,
-         0x24,
-         0x18,
-         0x24,
-         0x42,
-         0x81,
-         0x00,
-         0x00,
-         0x00,
-         0x00,
-         0x00,
-         0x00,
-         0x00,
-         0x00,
-         0x00,
-         0x00,
-         0x00,
-         0x00,
-         0x00,
-         0x00,
-         0x00,
-         0x00,
-     },
-     ui::Color::yellow().v,
-     app_location_t::GAMES,
-     -1,
- 
-     {0, 0, 0, 0},
-     0x00000000,
- };
- }  // namespace ui::external_app::spaceinv
- 
+#include "ui.hpp"
+#include "ui_spaceinv.hpp"
+#include "ui_navigation.hpp"
+#include "external_app.hpp"
+
+namespace ui::external_app::spaceinv {
+void initialize_app(ui::NavigationView& nav) {
+    nav.push<SpaceInvadersView>();
+}
+}  // namespace ui::external_app::spaceinv
+
+extern "C" {
+
+__attribute__((section(".external_app.app_spaceinv.application_information"), used)) application_information_t _application_information_spaceinv = {
+    (uint8_t*)0x00000000,
+    ui::external_app::spaceinv::initialize_app,
+    CURRENT_HEADER_VERSION,
+    VERSION_MD5,
+
+    "Space Invaders",
+    {
+        0x18,
+        0x3C,
+        0x7E,
+        0xDB,
+        0xFF,
+        0xFF,
+        0x24,
+        0x66,
+        0x42,
+        0x00,
+        0x24,
+        0x18,
+        0x24,
+        0x42,
+        0x81,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+    },
+    ui::Color::yellow().v,
+    app_location_t::GAMES,
+    -1,
+
+    {0, 0, 0, 0},
+    0x00000000,
+};
+}  // namespace ui::external_app::spaceinv
