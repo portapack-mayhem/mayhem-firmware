ee2273c53a2b2c89a66c0e4be0e264061128fbab
Netro <baginski.tj@gmail.com>
2025-06-21 20:49:22 -0400

    Fix Copyright

--- firmware/application/apps/ble_rx_app.cpp
+++ firmware/application/apps/ble_rx_app.cpp
@@ -2,6 +2,7 @@
  * Copyright (C) 2014 Jared Boone, ShareBrained Technology, Inc.
  * Copyright (C) 2017 Furrtek
  * Copyright (C) 2023 TJ Baginski
+ * Copyright (C) 2025 Tommaso Ventafridda
  *
  * This file is part of PortaPack.
  *
