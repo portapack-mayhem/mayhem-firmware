a557a6c78974b58d44baa62e82ec1960087dee75
StarVore <mspizzarolls4@gmail.com>
2025-11-22 14:48:32 -0500

    reverted changes to SSTVTX

--- firmware/baseband/proc_sstvtx.cpp
+++ firmware/baseband/proc_sstvtx.cpp
@@ -23,7 +23,6 @@
 #include "proc_sstvtx.hpp"
 #include "sine_table_int8.hpp"
 #include "event_m4.hpp"
-#include "audio_dma.hpp"
 
 #include <cstdint>
 
@@ -150,7 +149,6 @@ void SSTVTXProcessor::on_message(const Message* const msg) {
 }
 
 int main() {
-    audio::dma::init_audio_out();
     EventDispatcher event_dispatcher{std::make_unique<SSTVTXProcessor>()};
     event_dispatcher.run();
     return 0;
