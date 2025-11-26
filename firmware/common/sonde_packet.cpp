63e6dd25898fda6f9248b17dc2a3cfc18a66b797
HTotoo <ttotoo@gmail.com>
2025-11-26 11:31:03 +0100

    Add support for Meteomodem M20 temperature and humidity readings

--- firmware/common/sonde_packet.cpp
+++ firmware/common/sonde_packet.cpp
@@ -160,6 +160,8 @@ uint32_t Packet::frame() const {
     if (type_ == Type::Vaisala_RS41_SG) {
         uint32_t frame_number = vaisala_descramble(pos_FrameNb) | (vaisala_descramble(pos_FrameNb + 1) << 8);
         return frame_number;
+    } else if (type_ == Type::Meteomodem_M20) {
+        return reader_bi_m.read(0x15 * 8, 8);
     } else {
         return 0;  // Unknown
     }
@@ -331,6 +333,78 @@ temp_humid Packet::get_temp_humid() const {
             result.humid = rh;
         }
     }
+
+    if (type_ == Type::Meteomodem_M20) {
+        float p0 = 1.07303516e-03,
+              p1 = 2.41296733e-04,
+              p2 = 2.26744154e-06,
+              p3 = 6.52855181e-08;
+        float Rs[3] = {12.1e3, 36.5e3, 475.0e3};  // bias/series
+        float Rp[3] = {1e20, 330.0e3, 2000.0e3};  // parallel, Rp[0]=inf
+        uint8_t scT = 0;                          // {0,1,2}, range/scale voltage divider
+        uint16_t ADC_RT;                          // ADC12
+        // ui16_t Tcal[2];
+
+        float x, R;
+        float T = 0;  // T/Kelvin
+        uint32_t b2 = reader_bi_m.read(0x5 * 8, 8);
+        uint32_t b1 = reader_bi_m.read(0x4 * 8, 8);
+        ADC_RT = (b2 << 8) | b1;
+        if (ADC_RT > 8191) {
+            scT = 2;
+            ADC_RT -= 8192;
+        } else if (ADC_RT > 4095) {
+            scT = 1;
+            ADC_RT -= 4096;
+        } else {
+            scT = 0;
+        }  // also if (ADC_RT>>12)&3 == 3
+        // ADC12 , 4096 = 1<<12, max: 4095
+        x = (4095.0 - ADC_RT) / ADC_RT;  // (Vcc-Vout)/Vout = Vcc/Vout - 1
+        R = Rs[scT] / (x - Rs[scT] / Rp[scT]);
+        if (R > 0) T = 1.0 / (p0 + p1 * log(R) + p2 * log(R) * log(R) + p3 * log(R) * log(R) * log(R));
+        if (T - 273.15 < -120.0 || T - 273.15 > 60.0) T = 0;  // T < -120C, T > 60C invalid
+        result.temp = T - 273.15;                             // celsius
+
+        // humidity
+        // humi helper tntc2:
+        float Rsq = 22.1e3;         // P5.6=Vcc
+        float R25 = 2.2e3;          // 0.119e3; //2.2e3;
+        float b = 3650.0;           // B/Kelvin
+        float T25 = 25.0 + 273.15;  // T0=25C, R0=R25=5k
+        // -> Steinhart-Hart coefficients (polyfit):
+        T = 0.0;            // T/Kelvin
+        uint16_t ADC_ntc0;  // M10: ADC12 P6.4(A4)
+        float xq, Rq;
+        uint32_t bq2 = reader_bi_m.read(0x7 * 8, 8);
+        uint32_t bq1 = reader_bi_m.read(0x6 * 8, 8);
+        ADC_ntc0 = (bq2 << 8) | bq1;          // M10: 0x40,0x3F
+        xq = (4095.0 - ADC_ntc0) / ADC_ntc0;  // (Vcc-Vout)/Vout
+        Rq = Rsq / xq;
+        if (Rq > 0) T = 1.0 / (1.0 / T25 + 1.0 / b * log(Rq / R25));
+
+        // really the humidity
+        float TU = T - 273.15;
+        float RH = -1.0f;
+        float xqq;
+
+        uint16_t humval = ((uint32_t)reader_bi_m.read(0x03 * 8, 8) << 8) | (uint32_t)reader_bi_m.read(0x02 * 8, 8);
+        uint16_t rh_cal = ((uint32_t)reader_bi_m.read(0x30 * 8, 8) << 8) | (uint32_t)reader_bi_m.read(0x2F * 8, 8);
+        float humidityCalibration = 6.4e8f / (rh_cal + 80000.0f);
+        xqq = (humval + 80000.0f) * humidityCalibration * (1.0f - 5.8e-4f * (TU - 25.0f));
+        xqq = 4.16e9f / xqq;
+        xqq = 10.087f * xqq * xqq * xqq - 211.62f * xqq * xqq + 1388.2f * xqq - 2797.0f;
+        RH = -1.0f;
+        if (humval < 48000) {
+            if (xqq > -20.0f && xqq < 120.f) {
+                RH = xqq;
+                if (RH < 0.0f) RH = 0.0f;
+                if (RH > 100.0f) RH = 100.0f;
+            }
+        }
+        result.humid = RH;
+    }
+
     return result;
 }
 
