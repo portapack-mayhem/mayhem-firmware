ca485207ab1d0f1674b8f8dfb6c427a2611155c7
HTotoo <ttotoo@gmail.com>
2026-01-23 13:30:47 +0100

    fix

--- firmware/application/apps/ui_aprs_tx.cpp
+++ firmware/application/apps/ui_aprs_tx.cpp
@@ -222,7 +222,7 @@ APRSTXView::APRSTXView(NavigationView& nav) {
                 process_coordinates(lat, lon);
             });
     };
-    process_coordinates(last_lat, last_lon);
+    // process_coordinates(last_lat, last_lon); //don't load last, so won't confuse users
 }
 
 } /* namespace ui */
