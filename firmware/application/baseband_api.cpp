2f458822bc553eeee0539c81c77815b60dbe09de
fuwenbo <fuwenbo95@126.com>
2026-02-19 21:01:53 +0800

    修复RX AUDIO模式切换baseband sync fail

--- firmware/application/baseband_api.cpp
+++ firmware/application/baseband_api.cpp
@@ -473,7 +473,10 @@ void shutdown() {
     send_message(&message);
 
     shared_memory.application_queue.reset();
-
+    // add some time for the message to be processed and baseband to shut down before allowing another image to be run, otherwise the M4 may still be running and cause a crash when the next image is run.
+#ifdef PRALINE
+    chThdSleepMilliseconds(20);
+#endif
     baseband_image_running = false;
 }
 
