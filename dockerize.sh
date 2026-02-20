087e9f49ec0b2fa45ab5e8cd3376996ce9b42839
Eisenberger Tamas <e.tamas@iwstudio.hu>
2026-02-20 14:14:21 +0100

    Update dockerize documentation

--- dockerize.sh
+++ dockerize.sh
@@ -15,8 +15,11 @@
 # - Get a shell inside the build image to
 #   inspect problems: ./dockerize.sh shell
 # - Give additional parameters to the container:
-#   ./dockerize.sh -j10
-#   ./dockerize.sh ninja -j10
+#   Additional parameters to make:        ./dockerize.sh -j10
+#   Use ninja instead of make:            ./dockerize.sh ninja -j10
+#   Addotopnal CMake parameters:          ./dockerize.sh -DBOARD=praline
+#   Change the default build directory:   ./dockerize.sh -B build-praline -DBOARD=praline
+#                                         (Alternatively --workdir works as well)
 # - Use a different dockerfile:
 #   ./dockerize.sh build dockerfile-other
 # - Use a different cpu architecture:
