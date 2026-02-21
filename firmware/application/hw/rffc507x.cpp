c8a529576f516fabfc026e1732d49e933cba6deb
Sergio Tafur <setafur@gmail.com>
2026-02-20 22:36:13 -0500

    Updated rffc507xx.cpp, and ensured hackrf submodule at f1dcd554.

--- firmware/application/hw/rffc507x.cpp
+++ firmware/application/hw/rffc507x.cpp
@@ -204,8 +204,7 @@ void RFFC507x::init() {
     gpio_rffc5072_enx.clear();  // ENX=0 (enabled)
 
     // Small delay for chip to power up
-    for (volatile int i = 0; i < 10000; i++) {
-    }
+    chThdSleepMilliseconds(1);
 #else
     gpio_rffc5072_resetx.set();
     gpio_rffc5072_resetx.output();
