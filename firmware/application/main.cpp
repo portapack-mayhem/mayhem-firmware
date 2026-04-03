6bfa51078d93a5594a93d842fd3ece759c3fa3f5
jLynx <admin@jlynx.net>
2026-04-04 12:14:07 +1300

    Add device subdirectory support for APPS layout in firmware and workflows

--- firmware/application/main.cpp
+++ firmware/application/main.cpp
@@ -137,6 +137,7 @@ Continuous (Fox-oring)
 #include "gcc.hpp"
 
 #include "sd_card.hpp"
+#include "file_path.hpp"
 
 #include <string.h>
 #include "i2cdevmanager.hpp"
@@ -199,6 +200,7 @@ int main(void) {
 
         case portapack::init_status_t::INIT_SUCCESS:
 
+            init_apps_dir();
             config_mode_clear();
 
             lcd_frame_sync_configure();
