ab0a57d6b845755e8e04b45e443232ce42fe76ab
Pezsma <pezsma15@gmail.com>
2026-04-07 19:20:21 +0200

    V2.1

--- firmware/application/hw/max2831.cpp
+++ firmware/application/hw/max2831.cpp
@@ -481,9 +481,10 @@ void MAX2831::write(const address_t reg_num, const reg_t value) {
 
 void MAX2831::set_rssi_mux(const uint8_t mode) {
     /* RSSI MUX allows switching the RSSI output between different internal signals.
-     * 0 = RSSI
-     * 1 = TEMP
-     * 2 = TX_POWER
+     * 0 = disable mux (0V output)
+     * 1 = RSSI
+     * 2 = TEMP
+     * 3 = TX_POWER
      */
     uint16_t mux_val = 0;
 
