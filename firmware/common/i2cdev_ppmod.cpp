c9f310f548b4b38476145bc81846621d730e8323
Totoo <ttotoo@gmail.com>
2026-04-23 13:18:56 +0200

    Add power off command to I2cDev_PPmod class (#3152)

--- firmware/common/i2cdev_ppmod.cpp
+++ firmware/common/i2cdev_ppmod.cpp
@@ -242,4 +242,12 @@ std::vector<uint8_t> I2cDev_PPmod::downloadStandaloneApp(uint32_t index, size_t 
     return ret;
 }
 
+bool I2cDev_PPmod::send_poweroff_command() {
+    if (isLocked()) return false;  // device is busy
+    Command cmd = Command::COMMAND_POWER_OFF;
+    uint8_t tmp = 0;  // result of the ack. 0 = no, 1 = ok
+    i2c_read((uint8_t*)&cmd, 2, &tmp, 1);
+    return (tmp == 1);
+}
+
 }  // namespace i2cdev
