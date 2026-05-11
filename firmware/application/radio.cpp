56a4167478f1c4c85c0d368e34df5c9f189a04ad
HTotoo <ttotoo@gmail.com>
2026-05-11 13:13:10 +0200

    drain fix

--- firmware/application/radio.cpp
+++ firmware/application/radio.cpp
@@ -413,15 +413,15 @@ void set_rx_max283x_iq_phase_calibration(const size_t v) {
 }
 
 void disable() {
-    static constexpr uint32_t radio_tx_drain_timeout_ms = 100;
-
-    shared_memory.radio_tx_drain = 1;  // Request drain of the current DMA queue.
-    for (uint32_t waited_ms = 0;
-         shared_memory.radio_tx_drain && (waited_ms < radio_tx_drain_timeout_ms);
-         ++waited_ms) {
-        chThdSleepMilliseconds(1);
+    if (direction == rf::Direction::Transmit) {
+        static constexpr uint32_t radio_tx_drain_timeout_ms = 100;
+        shared_memory.radio_tx_drain = 1;  // Request drain of the current DMA queue.
+        for (uint32_t waited_ms = 0;
+             shared_memory.radio_tx_drain && (waited_ms < radio_tx_drain_timeout_ms);
+             ++waited_ms) {
+            chThdSleepMilliseconds(1);
+        }
     }
-
     /* Never allow shutdown to block indefinitely waiting for a drain
      * acknowledgement that may never arrive in normal operation. */
     shared_memory.radio_tx_drain = 0;
--- firmware/baseband/baseband_thread.cpp
+++ firmware/baseband/baseband_thread.cpp
@@ -90,6 +90,7 @@ void BasebandThread::run() {
 #ifdef PRALINE
     shared_memory.m4_streaming_marker = 0xAA;  // Phase 0 instrumentation
 #endif
+    uint8_t buffer_drained = 0;  // to count how many dma buffers we already emptied
     while (!chThdShouldTerminate()) {
 #ifdef PRALINE
         shared_memory.m4_baseband_loops++;  // Phase 0 instrumentation
@@ -120,8 +121,13 @@ void BasebandThread::run() {
                     baseband_processor_->execute(buffer);
                 }
             }
-        } else {
-            shared_memory.radio_tx_drain = 0;  // signal, that we have drained it. not a problem if after this we'll generate new ones, those won't matter.
+            if (shared_memory.radio_tx_drain == 1) {
+                buffer_drained++;
+                if (buffer_drained >= 4) {             // We have 4 buffers, so after draining 4 we should be safe to disable.
+                    shared_memory.radio_tx_drain = 0;  // Clear the drain request to allow normal operation to resume.
+                    buffer_drained = 0;
+                }
+            }
         }
     }
     shared_memory.radio_tx_drain = 0;
