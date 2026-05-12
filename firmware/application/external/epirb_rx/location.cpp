c34a6940ca8bc0eebbda141099b4169203be9fc0
Frederic BORRY <fborry@free.fr>
2026-05-12 14:00:47 +0200

    EPIRB RX app rework + EPIRB TX app fixes (#3172)
    * First step to EPIRB rx app fix
    * Added spike filtering on carrier detection for more robustness.
    * Created dedicated packet builder
    * First step to fixing UI.
    * First step to epirb-rx ui rework.
    * Made static methods static members
    * Code optim
    * Code cleanup + first step to new UI
    * Fixed display
    * Added BeaconList widget + BeaconDB
    * Epirb TX app memory optim
    * Fixed OOM when opening Map Display or Locator editor on EPIRB TX app
    * First step to tabs + code cleanup to free some space...
    * Added spectrum analyzer + fixed display
    * Fixed display refresh issue after leaving spectrum.
    * More code optim + first step to detail view.
    * Fixed float coord display.
    * Fixed protocol names
    * Fixed location display
    * Externalized country base to sd card + more code optim
    * Fixed country display
    * Fixed path
    * Added protocol description and resource manager
    * Added main/aux loc device
    * Removed specan to save space
    * First step to adding QR code
    * Used a custom TextArea component instead of Console
    * Fixed QR code display for map URL
    * Added beacon selection management + fixes
    * First step to map/data qr.
    * QR tab : added data + detail/map toggle
    * Removed sqhelch to save space
    * Removed audio to save space
    * Removed freq label to save space
    * Removed packet timestamp to save space
    * Moved frequency values to resource file
    * Fixed detection parameters to suite real beacons (slower phase shift time).
    * Fixed countdown field size
    * Fixed frequency list.
    * Fixed detail view on select.
    * First step to fix settings.
    * Fixed squelch for a 0-99 range.
    * Added bch code correction (single bit error).
    * More code optim, disable country cache, removed inline from location to reduce code size.
    * Removed inline, code formatting.
    * Added bch code correction display.
    * Fixed focus bug, fixed packet size check. Added BCH1/BCH2 correction test cases in BEACONS.TXT file for EPIRB TX application.
    * Fixed merge
    * Added countdown setting.
    * Added selection display to beacon list
    * Restored squelch control. Added countdown reset.
    * Code optim on formatSummary
    * More code optim
    * Made ResourceManager methods static
    * Externalized beacon strings to res file
    *Removed frame parameter from methods
    * Added test beacons for emergency. Removed old res file.
    * Fixed resource manager
    * Fixed resources. Added emergency display.
    * BCH + res fixes
    * Fixed beacons
    * Fixed app color. Fixed HexId and Serial calculation.
    * Fixed frequency list. Comments.
    * Code cleanup and comments
    * Added slideshow mode to EPIRB TX application. Code comments / cleanup.
    * Fixed frame end after wrong code cleanup
    * More code optim and comments. Removed touch from beacon list.
    * Added beacon selection with encoder on detail view.
    * Added beacon paging in header. Fixed timeout display
    * Fixed build for PRALINE
    * Fixed formatting
    * Potential fix for pull request finding
    * stop on carriage return
    * empty string if unknown
    * zeroing data buffer before use
    * fix typo
    * add include
    * fix case if pair == 0
    * fix scpectrum_on to spectrum_on
    * fix typos
    * replaced 3 inlined offset blocks with apply_offset calls
    * set_beacon consolidation

---
+++ firmware/application/external/epirb_rx/location.cpp
@@ -0,0 +1,149 @@
+/*
+ * Copyright (C) 2026 Frederic BORRY - ADRASEC 31
+ *
+ * This file is part of PortaPack.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "location.hpp"
+#include <cstdio>
+#include "string_format.hpp"
+
+namespace ui::external_app::epirb_rx {
+
+void Location::Angle::clear() {
+    degrees = 255;
+    minutes = 0;
+    seconds = 0;
+    orientation = false;
+    floatValue = 255;
+}
+
+float Location::Angle::getFloatValue() {
+    if (floatValue >= 255) {
+        floatValue = (float)degrees;
+        floatValue += ((float)minutes / 60.0f);
+        floatValue += ((float)seconds / 3600.0f);
+        if (orientation) {
+            floatValue = -floatValue;
+        }
+    }
+    return floatValue;
+}
+
+void Location::Angle::apply_offset(bool positive, long ofmin, long ofsec) {
+    if (positive) {
+        minutes += ofmin;
+        seconds += ofsec;
+    } else {
+        minutes -= ofmin;
+        if (minutes < 0) {
+            minutes += 60;
+            degrees -= 1;
+        }
+        seconds -= ofsec;
+        if (seconds < 0) {
+            seconds += 60;
+            minutes -= 1;
+        }
+    }
+}
+
+void Location::clear() {
+    latitude.clear();
+    longitude.clear();
+}
+
+bool Location::isUnknown() {
+    return (latitude.degrees >= 255 || longitude.degrees >= 255);
+}
+
+char Location::gps_letterize(int x) {
+    return (char)x + 65;
+}
+
+size_t Location::gps_compute_locator(char* buffer, float lat, float lon, int precision) {
+    size_t result = 0;
+
+    lon += 180.0f;
+    lat += 90.0f;
+
+    int A = lon / 20;
+    int B = lat / 10;
+
+    lon -= A * 20;
+    lat -= B * 10;
+
+    int C = lon / 2;
+    int D = lat / 1;
+
+    lon -= C * 2;
+    lat -= D * 1;
+
+    int E = lon / (5.0f / 60.0f);
+    int F = lat / (2.5f / 60.0f);
+
+    lon -= E * (5.0f / 60.0f);
+    lat -= F * (2.5f / 60.0f);
+
+    int G = lon / (5.0f / 600.0f);
+    int H = lat / (2.5f / 600.0f);
+
+    buffer[result++] = char('A' + A);
+    buffer[result++] = char('A' + B);
+
+    if (precision >= 4) {
+        buffer[result++] = char('0' + C);
+        buffer[result++] = char('0' + D);
+    }
+
+    if (precision >= 6) {
+        buffer[result++] = char('a' + E);
+        buffer[result++] = char('a' + F);
+    }
+
+    if (precision >= 8) {
+        buffer[result++] = char('0' + G);
+        buffer[result++] = char('0' + H);
+    }
+    buffer[result] = 0;
+    return result;
+}
+
+size_t Location::toString(char* buffer, LocationFormat format, int precision) {
+    if (isUnknown()) {
+        return sprintf(buffer, "%s", "GPS not synchronized");
+    }
+    switch (format) {
+        case LocationFormat::SEXAGESIMAL: {
+            return sprintf(buffer, "%ld\xB0%02ld'%02ld\"%c, %ld\xB0%02ld'%02ld\"%c",  // 0xB0 is degree ° symbol in our 8x16 font
+                           latitude.degrees, latitude.minutes, latitude.seconds, latitude.orientation ? 'S' : 'N',
+                           longitude.degrees, longitude.minutes, longitude.seconds, longitude.orientation ? 'W' : 'E');
+        }
+        case LocationFormat::MAIDENHEAD_LOCATOR:
+            return gps_compute_locator(buffer, latitude.getFloatValue(), longitude.getFloatValue(), precision);
+        default:
+        case LocationFormat::DECIMAL:
+            return sprintf(buffer, "%s, %s", to_string_decimal(latitude.getFloatValue(), 5).c_str(), to_string_decimal(longitude.getFloatValue(), 5).c_str());
+    }
+}
+
+void Location::formatFloatLocation(char* buffer, const char* format) {
+    sprintf(buffer, format, to_string_decimal(latitude.getFloatValue(), 6).c_str(), to_string_decimal(longitude.getFloatValue(), 6).c_str());
+}
+
+}  // namespace ui::external_app::epirb_rx
---
+++ firmware/application/external/epirb_rx/ui_beaconlist.hpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2026 Frederic BORRY - ADRASEC 31
+ *
+ * This file is part of PortaPack.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __UI_BEACONLIST_H__
+#define __UI_BEACONLIST_H__
+
+#include "ui.hpp"
+#include "ui_painter.hpp"
+#include "ui_widget.hpp"
+#include "beacon_db.hpp"
+
+namespace ui::external_app::epirb_rx {
+
+/**
+ * Beacon list component
+ */
+class BeaconUIList : public View {
+   public:
+    std::function<void(size_t)> on_select{};
+
+    BeaconUIList(Rect parent_rect);
+    BeaconUIList(const BeaconUIList& other) = delete;
+    BeaconUIList& operator=(const BeaconUIList& other) = delete;
+
+    void paint(Painter& painter) override;
+    void on_show() override;
+    bool on_key(const KeyEvent key) override;
+    bool on_encoder(EncoderEvent delta) override;
+
+    size_t get_index() const;
+    void set_db(BeaconDB& db);
+
+   private:
+    void adjust_selected_index(int index);
+
+    BeaconDB* db_{nullptr};
+    size_t start_index_{0};
+    size_t selected_index_{0};
+};
+
+}  // namespace ui::external_app::epirb_rx
+
+#endif /*__UI_BEACONLIST_H__*/
